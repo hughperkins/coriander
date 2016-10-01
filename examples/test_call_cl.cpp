f4fc57dd234db9dda05065a9e2e47c108928d7ac
Hugh Perkins <hughperkins@gmail.com>
2016-10-02 00:37:39 +0100

    dabbling in adding args

--- examples/test_call_cl.cpp
+++ examples/test_call_cl.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 void launchSetValue(float *data, int idx, float value);
 
+size_t grid[3];
+size_t block[3];
 unique_ptr<CLKernel> kernel;
 unique_ptr<EasyCL> cl;
 
@@ -23,10 +25,41 @@ void pretendLaunch(
     cout << "pretendLaunch(" << kernelName << ")" << endl;
     cout << "grid(" << grid_x << ", " << grid_y << ", " << grid_z << ")" << endl;
     cout << "block(" << block_x << ", " << block_y << ", " << block_z << ")" << endl;
-
+    grid[0] = grid_x;
+    grid[1] = grid_y;
+    grid[2] = grid_z;
+    block[0] = block_x;
+    block[1] = block_y;
+    block[2] = block_z;
     // lets just read the kernel from file for now, with hardcoded filename
     // kernel.reset(buildKernelFromString("", kernelName, "", "internal"));
     kernel.reset(cl->buildKernel("examples/testcudakernel1.cl", kernelName, ""));
+}
+
+void setKernelArgMem(float *clmem_as_floatstar) {
+    cl_mem *p_mem = (cl_mem *)clmem_as_floatstar;
+    cout << "setkernelargmem" << endl;
+    kernel->inout(p_mem);
+}
+
+void setKernelArgInt(int value) {
+    // cl_mem *p_mem = (cl_mem *)clmem_as_floatstar;
+    cout << "setkernelargint " << value << endl;
+    kernel->in(value);
+}
+
+void setKernelArgFloat(float value) {
+    // cl_mem *p_mem = (cl_mem *)clmem_as_floatstar;
+    cout << "setkernelargfloat " << value << endl;
+    kernel->in(value);
+}
+
+void kernelGo() {
+    size_t global[3];
+    for(int i = 0; i < 3; i++) {
+        global[i] = grid[i] * block[i];
+    }
+    kernel->run(3, global, block);
 }
 
 int main(int argc, char *argv[]) {
