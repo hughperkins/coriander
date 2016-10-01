a7d6ac88738f59e7bab04af27ac33e93e8c03194
Hugh Perkins <hughperkins@gmail.com>
2016-10-01 20:36:49 +0100

    pondeirng kernel launch. migrating to use easycl

--- examples/test_call_cl.cpp
+++ examples/test_call_cl.cpp
@@ -2,6 +2,9 @@
 // this needs opencl installed, including working opencl gpu and drivers
 
 #include <iostream>
+#include <memory>
+
+#include "EasyCL.h"
 
 #include "CL/cl.h"
 
@@ -10,50 +13,55 @@ using namespace std;
 void launchSetValue(float *data, int idx, float value);
 
 int main(int argc, char *argv[]) {
+    // cl_int err;
+    // cl_platform_id platform = 0;
+    // cl_device_id device = 0;
+    // cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
+    // cl_context ctx = 0;
+    // cl_command_queue queue = 0;
+    // // cl_mem bufA, bufB, bufC;
+    // // cl_event event = NULL;
+
+    // err = clGetPlatformIDs(1, &platform, NULL);
+    // if (err != CL_SUCCESS) {
+    //     cout << "clGetPlatformIDs() failed with " << err << endl;
+    //     return 1;
+    // }
+    // cout << "got platforms" << endl;
+
+    // err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+    // if (err != CL_SUCCESS) {
+    //     cout << "clGetPlatformIDs() failed with " << err << endl;
+    //     return 1;
+    // }
+    // cout << "got device" << endl;
+
+    // props[1] = (cl_context_properties)platform;
+    // ctx = clCreateContext(props, 1, &device, NULL, NULL, &err);
+    // if (err != CL_SUCCESS) {
+    //     cout << "clCreateContext() failed with " << err << endl;
+    //     return 1;
+    // }
+    // cout << "got context" << endl;
+
+    // queue = clCreateCommandQueue(ctx, device, 0, &err);
+    // if (err != CL_SUCCESS) {
+    //     cout << "clCreateCommandQueue() failed with " << err << endl;
+    //     clReleaseContext(ctx);
+    //     return 1;
+    // }
+    // cout << "created queue" << endl;
+
+    unique_ptr<EasyCL> cl(EasyCL::createForFirstGpuOtherwiseCpu());
     cl_int err;
-    cl_platform_id platform = 0;
-    cl_device_id device = 0;
-    cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
-    cl_context ctx = 0;
-    cl_command_queue queue = 0;
-    // cl_mem bufA, bufB, bufC;
-    // cl_event event = NULL;
-
-    err = clGetPlatformIDs(1, &platform, NULL);
-    if (err != CL_SUCCESS) {
-        cout << "clGetPlatformIDs() failed with " << err << endl;
-        return 1;
-    }
-    cout << "got platforms" << endl;
-
-    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
-    if (err != CL_SUCCESS) {
-        cout << "clGetPlatformIDs() failed with " << err << endl;
-        return 1;
-    }
-    cout << "got device" << endl;
-
-    props[1] = (cl_context_properties)platform;
-    ctx = clCreateContext(props, 1, &device, NULL, NULL, &err);
-    if (err != CL_SUCCESS) {
-        cout << "clCreateContext() failed with " << err << endl;
-        return 1;
-    }
-    cout << "got context" << endl;
-
-    queue = clCreateCommandQueue(ctx, device, 0, &err);
-    if (err != CL_SUCCESS) {
-        cout << "clCreateCommandQueue() failed with " << err << endl;
-        clReleaseContext(ctx);
-        return 1;
-    }
-    cout << "created queue" << endl;
+    cl_context *ctx = cl->context;
+    cl_command_queue *queue = cl->queue;
 
     int N = 1024;
     float *float_data = new float[N];
-    cl_mem float_data_gpu = clCreateBuffer(ctx, CL_MEM_READ_ONLY, N * sizeof(float),
+    cl_mem float_data_gpu = clCreateBuffer(*ctx, CL_MEM_READ_ONLY, N * sizeof(float),
                                            NULL, &err);
-    err = clEnqueueWriteBuffer(queue, float_data_gpu, CL_TRUE, 0,
+    err = clEnqueueWriteBuffer(*queue, float_data_gpu, CL_TRUE, 0,
                                       N * sizeof(float), float_data, 0, NULL, NULL);
 
     launchSetValue((float *)float_data_gpu, 2, 123.0f);
--- run-test-call-cl.sh
+++ run-test-call-cl.sh
@@ -10,12 +10,29 @@ COMPILE_FLAGS="$(llvm-config-3.8 --cxxflags) -std=c++11"
 LINK_FLAGS="$(llvm-config-3.8 --ldflags --system-libs --libs all)"
 
 mkdir -p build
+
+# build EasyCL
+git submodule update --init --recursive
+cd build
+cmake ../src/EasyCL -DCMAKE_INSTALL_PREFIX=${PWD} -DBUILD_TESTS=ON
+make -j 4 install
+cd ..
+
+# build build/patch-hostside
 clang++-3.8 ${COMPILE_FLAGS} -fcxx-exceptions -o build/patch-hostside ir-to-opencl/ir-to-opencl-common.cpp -g -O3 -I/usr/include/llvm-3.8 ir-to-opencl/patch-hostside.cpp ${LINK_FLAGS}
+
+# convert the cuda file to IR
 clang++-3.8 -I${CUDA_HOME}/include examples/${TARGET}.cu --cuda-host-only -emit-llvm  -O3 -S -o build/${TARGET}-host.ll
+
+# run build/patch-hostside, to patch the IR
 build/patch-hostside build/${TARGET}-host.ll build/${TARGET}-host2.ll
+
+# compile the patched IR
 clang++-3.8 -c build/${TARGET}-host2.ll -O3 -o build/${TARGET}-host.o
 
-clang++-3.8 -I${CUDA_HOME}/include -c examples/test_call_cl.cpp --cuda-host-only -O3 -o build/test_call_cl.o
+# compile our main method
+clang++-3.8 -std=c++11 -Isrc/EasyCL -I${CUDA_HOME}/include -c examples/test_call_cl.cpp --cuda-host-only -O3 -o build/test_call_cl.o
 
+# link, and run
 g++ -o build/test_call_cl build/test_call_cl.o build/${TARGET}-host.o -L${CUDA_HOME}/lib64 -lcudart -lOpenCL
 LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH build/test_call_cl
