db5435be51200aee09dea8281253ebbcca0a9634
Hugh Perkins <hughperkins@gmail.com>
2016-10-02 00:20:26 +0100

    remove dead code

--- examples/test_call_cl.cpp
+++ examples/test_call_cl.cpp
@@ -23,45 +23,6 @@ void pretendLaunch(
 }
 
 int main(int argc, char *argv[]) {
-    // cl_int err;
-    // cl_platform_id platform = 0;
-    // cl_device_id device = 0;
-    // cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
-    // cl_context ctx = 0;
-    // cl_command_queue queue = 0;
-    // // cl_mem bufA, bufB, bufC;
-    // // cl_event event = NULL;
-
-    // err = clGetPlatformIDs(1, &platform, NULL);
-    // if (err != CL_SUCCESS) {
-    //     cout << "clGetPlatformIDs() failed with " << err << endl;
-    //     return 1;
-    // }
-    // cout << "got platforms" << endl;
-
-    // err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
-    // if (err != CL_SUCCESS) {
-    //     cout << "clGetPlatformIDs() failed with " << err << endl;
-    //     return 1;
-    // }
-    // cout << "got device" << endl;
-
-    // props[1] = (cl_context_properties)platform;
-    // ctx = clCreateContext(props, 1, &device, NULL, NULL, &err);
-    // if (err != CL_SUCCESS) {
-    //     cout << "clCreateContext() failed with " << err << endl;
-    //     return 1;
-    // }
-    // cout << "got context" << endl;
-
-    // queue = clCreateCommandQueue(ctx, device, 0, &err);
-    // if (err != CL_SUCCESS) {
-    //     cout << "clCreateCommandQueue() failed with " << err << endl;
-    //     clReleaseContext(ctx);
-    //     return 1;
-    // }
-    // cout << "created queue" << endl;
-
     unique_ptr<EasyCL> cl(EasyCL::createForFirstGpuOtherwiseCpu());
     cl_int err;
     cl_context *ctx = cl->context;
