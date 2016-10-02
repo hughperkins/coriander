6bc481a3cdcd9ce769e5a0dc1553a8f5ff450563
Hugh Perkins <hughperkins@gmail.com>
2016-10-02 15:43:27 +0100

    create dev-run-tests.sh, for maintainer usage

---
+++ dev-run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is for maintainer usage primarily: always builds ir-to-opencl first
+
+set -e
+
+export CUDA_HOME=/usr/local/cuda-7.5
+COMPILE_FLAGS="$(llvm-config-3.8 --cxxflags) -std=c++11"
+LINK_FLAGS="$(llvm-config-3.8 --ldflags --system-libs --libs all)"
+
+mkdir -p build
+
+# build build/ir-to-opencl
+clang++-3.8 ${COMPILE_FLAGS} -fcxx-exceptions -o build/ir-to-opencl -g -O3 -I/usr/include/llvm-3.8 src/ir-to-opencl.cpp src/ir-to-opencl-common.cpp ${LINK_FLAGS}
+
+test/test.sh
