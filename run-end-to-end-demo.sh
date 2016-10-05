e95c8f3a8f443d42006182fc7a40b9e4b71a8634
Hugh Perkins <hughperkins@gmail.com>
2016-10-05 08:35:56 +0100

    remember to update shell scripts after reorg

--- run-end-to-end-demo.sh
+++ run-end-to-end-demo.sh
@@ -10,16 +10,16 @@ COMPILE_FLAGS="$(llvm-config-3.8 --cxxflags) -std=c++11"
 LINK_FLAGS="$(llvm-config-3.8 --ldflags --system-libs --libs all)"
 
 # convert the cuda file to IR; this bit needs CUDA include files
-clang++-3.8 -I${CUDA_HOME}/include examples/${TARGET}.cu --cuda-host-only -emit-llvm  -O3 -S -o examples/generated/${TARGET}-host.ll
+clang++-3.8 -I${CUDA_HOME}/include test/${TARGET}.cu --cuda-host-only -emit-llvm  -O3 -S -o test/generated/${TARGET}-host.ll
 
 # run build/patch-hostside, to patch the IR
-build/patch-hostside examples/generated/${TARGET}-host.ll examples/generated/${TARGET}-host2.ll
+build/patch-hostside test/generated/${TARGET}-host.ll test/generated/${TARGET}-host2.ll
 
 # compile the patched IR
-clang++-3.8 -c examples/generated/${TARGET}-host2.ll -O3 -o build/${TARGET}-host.o
+clang++-3.8 -c test/generated/${TARGET}-host2.ll -O3 -o build/${TARGET}-host.o
 
 # compile our main method
-clang++-3.8 -std=c++11 -Isrc/EasyCL -c examples/test_call_cl.cpp --cuda-host-only -O3 -o build/test_call_cl.o
+clang++-3.8 -std=c++11 -Isrc/EasyCL -c test/test_call_cl.cpp --cuda-host-only -O3 -o build/test_call_cl.o
 
 # link, and run
 g++ -o build/test_call_cl build/test_call_cl.o build/${TARGET}-host.o -lOpenCL -Lbuild -lEasyCL
--- run-ir-to-opencl-demo.sh
+++ run-ir-to-opencl-demo.sh
@@ -7,7 +7,7 @@ export CUDA_HOME=/usr/local/cuda-7.5
 TARGET=testcudakernel1
 
 # convert incoming .cu file to device-side IR.  This bit needs CUDA include files
-clang++-3.8 -include include/fake_funcs.h -I${CUDA_HOME}/include examples/${TARGET}.cu --cuda-device-only -emit-llvm -O3 -S -o examples/generated/${TARGET}.ll
+clang++-3.8 -include include/fake_funcs.h -I${CUDA_HOME}/include test/${TARGET}.cu --cuda-device-only -emit-llvm -O3 -S -o test/generated/${TARGET}.ll
 
 # run ir-to-opencl, to convert the IR into OpenCL.  This bit only depends on clang, not on CUDA
-build/ir-to-opencl $1 examples/generated/${TARGET}.ll examples/generated/${TARGET}.cl
+build/ir-to-opencl $1 test/generated/${TARGET}.ll test/generated/${TARGET}.cl
