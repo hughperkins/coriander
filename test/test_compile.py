da922b877556775df5e238f2bbe59099bbdff2fe
Hugh Perkins <hughperkins@gmail.com>
2016-10-09 17:09:10 +0100

    remove an xfail (since passing now)

--- test/test_compile.py
+++ test/test_compile.py
@@ -9,7 +9,7 @@ import pytest
     "test/generated/struct_initializer-device.cl",
     pytest.mark.xfail("test/generated/phiaddressspace-device.cl"),
     pytest.mark.xfail("test/generated/pointerpointer-device.cl"),
-    pytest.mark.xfail("test/eigen/generated/test_cuda_elementwise_small-device.cl"),
+    "test/eigen/generated/test_cuda_elementwise_small-device.cl",
 ])
 def test_compile(context, cl_filepath):
     print(subprocess.check_output([
