64383e0afd4fd143fac92677ca44920993d2462a
Hugh Perkins <hughperkins@gmail.com>
2016-10-15 19:18:31 +0100

    push testevents.cu up, now its not hanging now (becuase of ret bugfix)

--- test/testevents.cu
+++ test/testevents.cu
@@ -14,13 +14,13 @@ __global__ void longKernel(float *data, int N, float value) {
 }
 
 int main(int argc, char *argv[]) {
-    int N = 128; // * 1024;
+    int N = 102400; // * 1024;
 
     float *floats;
     cuMemHostAlloc((void **)&floats, N * sizeof(float), CU_MEMHOSTALLOC_PORTABLE);
 
     // floats[2] = 4.0f;
-    longKernel<<<dim3(1, 1, 1), dim3(32, 1, 1)>>>(floats, N, 3.0f);
+    longKernel<<<dim3(102400 / 32, 1, 1), dim3(32, 1, 1)>>>(floats, N, 3.0f);
     cout << "queued kernel x" << endl;
 
     // cuCtxSynchronize();
