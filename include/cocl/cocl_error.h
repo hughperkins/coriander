ffa23d4f11ea95fe4656d943c24d2fe99250ba20
Hugh Perkins <hughperkins@gmail.com>
2016-10-20 12:00:08 +0100

    bunch of error symbols

--- include/cocl/cocl_error.h
+++ include/cocl/cocl_error.h
@@ -9,5 +9,29 @@ extern "C" {
 
 #define cudaErrorNotReady 2222
 
-#define CUDA_ERROR_NOT_READY cudaErrorNotReady
-#define CUDA_SUCCESS 0
+#define CUDA_SUCCESS 0  // this should probably be zero, by convention
+
+// the actual numbers for the others dont really matter, as long as we build all source code with same values
+// we start from 10000 so the numbers probably dont clash with the original NVIDIA numbers, so if someone accidentally
+// tries to link against code that uses the original numbers, it'll probalby go wrong more obviously than if the
+// the numbers are same but different...
+// (Note that there's no obvoius way to get the original numbers, without looking at the original toolkit files.
+// Whereas the names of the symbols are scattered liberally throughout the code of the projects we
+// are trying to build)
+enum cudaerrors {
+    CUDA_ERROR_INVALID_IMAGE = 10000,
+    CUDA_ERROR_INVALID_DEVICE,
+    CUDA_ERROR_INVALID_CONTEXT,
+    CUDA_ERROR_INVALID_HANDLE,
+    CUDA_ERROR_NOT_FOUND,
+    CUDA_ERROR_NOT_READY,
+    CUDA_ERROR_NOT_PERMITTED,
+    CUDA_ERROR_NOT_SUPPORTED
+    CUDA_ERROR_PEER_ACCESS_UNSUPPORTED,
+    CUDA_ERROR_ECC_UNCORRECTABLE,
+    CUDA_ERROR_NO_BINARY_FOR_GPU,
+    CUDA_ERROR_CONTEXT_ALREADY_IN_USE,
+    CUDA_ERROR_UNKNOWN,
+};
+
+#define cudaErrorNotReady CUDA_ERROR_NOT_READY
--- test/cocl/test_types.cu
+++ test/cocl/test_types.cu
@@ -2,7 +2,16 @@
 
 #include "cuda.h"
 
+#include <iostream>
+
+int returnerror() {
+    return CUDA_ERROR_INVALID_IMAGE;
+}
+
 int main(int argc, char *argv[]) {
     CUdevice device;
+    std::cout << returnerror() << std::endl;
+    std::cout << CUDA_ERROR_INVALID_IMAGE << std::endl;
+    std::cout << CUDA_ERROR_NOT_READY << std::endl;
     return 0;
 }
