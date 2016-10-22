b3cc4c93c907718d4e9a7b6f1402479a75b60d34
Hugh Perkins <hughperkins@gmail.com>
2016-10-22 17:40:28 +0100

    more bug squashing for offsets

--- src/cocl_memory.cpp
+++ src/cocl_memory.cpp
@@ -113,15 +113,19 @@ size_t cudaMemcpyAsync (void *dst, const void *src, size_t count, size_t cudaMem
     cl_int err;
     if(cudaMemcpyKind == cudaMemcpyDeviceToHost) {
         // device => host
-        Memory *srcMemory = (Memory *)src;
-        err = clEnqueueReadBuffer(queue->queue, srcMemory->clmem, CL_FALSE, 0,
+        // Memory *srcMemory = (Memory *)src;
+        Memory *srcMemory = findMemory((char *)src);
+        size_t src_offset = srcMemory->getOffset((char *)src);
+        err = clEnqueueReadBuffer(queue->queue, srcMemory->clmem, CL_FALSE, src_offset,
                                          count, dst, 0, NULL, NULL);
         cl->checkError(err);
         // cl->finish();
     } else if(cudaMemcpyKind == cudaMemcpyHostToDevice) {
         // host => device
-        Memory *dstMemory = (Memory*)dst;
-        err = clEnqueueWriteBuffer(queue->queue, dstMemory->clmem, CL_FALSE, 0,
+        // Memory *dstMemory = (Memory*)dst;
+        Memory *dstMemory = findMemory((char *)dst);
+        size_t dst_offset = dstMemory->getOffset((char *)dst);
+        err = clEnqueueWriteBuffer(queue->queue, dstMemory->clmem, CL_FALSE, dst_offset,
                                           count, src, 0, NULL, NULL);
         cl->checkError(err);
     } else {
@@ -143,17 +147,21 @@ size_t cudaMemsetAsync(void *devPtr, int value, size_t count, char *_queue) {
 
 size_t cuMemsetD8(CUdeviceptr location, unsigned char value, uint32_t count) {
     COCL_PRINT(cout << "cuMemsetD8 redirected value " << value << " count=" << count << endl);
-    Memory *memory = (Memory *)location;
+    // Memory *memory = (Memory *)location;
     // use default queue??
-    cl_int err = clEnqueueFillBuffer(cl->default_queue->queue, memory->clmem, &value, sizeof(unsigned char), 0, count * sizeof(unsigned char), 0, 0, 0);
+    Memory *memory = findMemory((char *)location);
+    size_t offset = memory->getOffset((char *)location);
+    cl_int err = clEnqueueFillBuffer(cl->default_queue->queue, memory->clmem, &value, sizeof(unsigned char), offset, count * sizeof(unsigned char), 0, 0, 0);
     cl->checkError(err);
     return 0;
 }
 
 size_t cuMemsetD32(CUdeviceptr location, unsigned int value, uint32_t count) {
-    Memory *memory = (Memory *)location;
+    // Memory *memory = (Memory *)location;
+    Memory *memory = findMemory((char *)location);
+    size_t offset = memory->getOffset((char *)location);
     COCL_PRINT(cout << "cuMemsetD32 redirected value " << value << " count=" << count << " location=" << location << " memory=" << (void *)memory << endl);
-    cl_int err = clEnqueueFillBuffer(cl->default_queue->queue, memory->clmem, &value, sizeof(int), 0, count * sizeof(int), 0, 0, 0);
+    cl_int err = clEnqueueFillBuffer(cl->default_queue->queue, memory->clmem, &value, sizeof(int), offset, count * sizeof(int), 0, 0, 0);
     cl->checkError(err);
     return 0;
 }
@@ -170,26 +178,22 @@ size_t cudaMemcpy(void *dst, const void *src, size_t bytes, size_t cudaMemcpyKin
     if(cudaMemcpyKind == cudaMemcpyDeviceToHost) {
         // device => host
         cout << "cudamemcpy device to host" << endl;
-        // Memory *srcMemory = (Memory *)src;
         Memory *srcMemory = findMemory((char *)src);
         size_t offset = srcMemory->getOffset((char *)src);
-        err = clEnqueueReadBuffer(cl->default_queue->queue, srcMemory->clmem, CL_TRUE, 0,
+        err = clEnqueueReadBuffer(cl->default_queue->queue, srcMemory->clmem, CL_TRUE, offset,
                                          bytes, dst, 0, NULL, NULL);
         cl->checkError(err);
         // cl->finish();
     } else if(cudaMemcpyKind == cudaMemcpyHostToDevice) {
         // host => device
         cout << "cudamemcpy host to device" << endl;
-        // Memory *dstMemory = (Memory *)dst;
         Memory *dstMemory = findMemory((char *)dst);
         size_t offset = dstMemory->getOffset((char *)dst);
-        err = clEnqueueWriteBuffer(cl->default_queue->queue, dstMemory->clmem, CL_TRUE, 0,
+        err = clEnqueueWriteBuffer(cl->default_queue->queue, dstMemory->clmem, CL_TRUE, offset,
                                           bytes, src, 0, NULL, NULL);
         cl->checkError(err);
     } else if(cudaMemcpyKind == cudaMemcpyDeviceToDevice) {
         // device => device
-        // Memory *srcMemory = (Memory *)src;
-        // Memory *dstMemory = (Memory *)dst;
         Memory *srcMemory = findMemory((char *)src);
         size_t src_offset = srcMemory->getOffset((char *)src);
         Memory *dstMemory = findMemory((char *)dst);
@@ -219,10 +223,6 @@ size_t cuMemcpyHtoDAsync(CUdeviceptr dst, const void *src, size_t bytes, char *_
     Memory *dstMemory = findMemory((char *)dst);
     size_t offset = dstMemory->getOffset((char *)dst);
     COCL_PRINT(cout << "memory " << (void *)dstMemory << " offset=" << offset << endl);
-    // cout << "cuMemcpyHtoDAsync cl->default_queue=" << cl->default_queue << endl;
-    // Memory *dstMemory = (Memory *)dst;
-    // cout << "cuMemcpyHtoDAsync dstMemory->clmem " << dstMemory->clmem << endl;
-    // cout << "cuMemcpyHtoDAsync cl->default_queue->queue=" << cl->default_queue->queue << endl;
     cl_int err = clEnqueueWriteBuffer(queue->queue, dstMemory->clmem, CL_FALSE, offset,
                                       bytes, src, 0, NULL, NULL);
     cl->checkError(err);
@@ -233,10 +233,8 @@ size_t  cuMemcpyDtoHAsync(void *dst, CUdeviceptr src, size_t bytes, char *_queue
     CLQueue *queue = (CLQueue*)_queue;
     COCL_PRINT(cout << "cuMemcpyDtoHAsync dst=" << dst << " src=" << src << " bytes=" << bytes << endl);
     Memory *srcMemory = findMemory((char *)src);
-    // size_t offset = (char *)src - (char *)srcMemory;
     size_t offset = srcMemory->getOffset((char *)src);
     COCL_PRINT(cout << "memory " << (void *)srcMemory << " offset=" << offset << endl);
-    // Memory *srcMemory = (Memory *)src;
     cl_int err = clEnqueueReadBuffer(queue->queue, srcMemory->clmem, CL_FALSE, offset,
                                      bytes, dst, 0, NULL, NULL);
     cl->checkError(err);
@@ -250,11 +248,6 @@ size_t cuMemcpyHtoD(CUdeviceptr gpu_dst, const void *host_src, size_t size) {
     return 0;
 }
 
-// // => synchronous <=
-// size_t cuMemcpyHtoD(CUdeviceptr gpu_dst, const void *host_src, size_t size) {
-//     return cuMemcpyHtoD(gpu_dst, host_src, size);
-// }
-
 // => synchronous <=
 size_t  cuMemcpyDtoH(void *host_dst, CUdeviceptr gpu_src, size_t size) {
     cout << "cumemcpyDtoH" << endl;
@@ -263,29 +256,19 @@ size_t  cuMemcpyDtoH(void *host_dst, CUdeviceptr gpu_src, size_t size) {
     return 0;
 }
 
-// // => synchronous <=
-// size_t  cuMemcpyDtoH(void *host_dst, CUdeviceptr gpu_src, size_t size) {
-//     cout << "cumemcpyDtoH" << endl;
-//     return cuMemcpyDtoH(host_dst, gpu_src, size);
-// }
-
 size_t cudaMalloc(void **_pMemory, size_t N) {
-    // Memory **pMemory = (Memory **)_pMemory;
     hostside_opencl_funcs_assure_initialized();
     Memory *memory = Memory::newDeviceAlloc(N);
     COCL_PRINT(cout << "cudaMalloc using cl, size " << N << " memory=" << (void *)memory << " fakePos=" << memory->fakePos << endl);
-    // *pMemory = memory;
     *_pMemory = (void *)memory->fakePos;
     return 0;
 }
 
 size_t cuMemAlloc(CUdeviceptr*_pMemory, size_t bytes) {
-    // Memory **pMemory = (Memory **)_pMemory;
     return cudaMalloc((void **)_pMemory, bytes);
 }
 
 size_t cudaFree(void *_memory) {
-    // Memory *memory = (Memory *)_memory;
     Memory *memory = findMemory((char *)_memory);
     COCL_PRINT(cout << "cudafree using opencl memory=" << memory << endl);
     delete memory;
@@ -293,6 +276,5 @@ size_t cudaFree(void *_memory) {
 }
 
 size_t cuMemFree(CUdeviceptr memory) {
-    // Memory *pMemory = (Memory *)_memory;
     return cudaFree((void *)memory);
 }
