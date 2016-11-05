4050e5a5ca1cb968c092f6bcd63f0d9675e90e26
Hugh Perkins <hughperkins@gmail.com>
2016-11-05 20:48:24 +0000

    add build comment for cocl outputs to makelists

--- cmake/cocl.cmake
+++ cmake/cocl.cmake
@@ -1,6 +1,7 @@
 macro(add_cocl_executable OUTPUT SRCFILE)
     add_custom_command(
         OUTPUT ${OUTPUT}
+        COMMENT "${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT}"
         COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT}
         DEPENDS cocl clblast clew easycl
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
