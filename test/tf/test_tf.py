6d5f0e142c99c6065deedb27b079a70ea86d7adc
Hugh Perkins <hughperkins@gmail.com>
2016-11-08 08:44:48 +0000

    test_tf this test case passes

--- test/tf/test_tf.py
+++ test/tf/test_tf.py
@@ -5,6 +5,7 @@ Check does/doesnt work
 import numpy as np
 import pyopencl as cl
 import subprocess
+import math
 from test import test_common
 from test.test_common import compile_code
 
@@ -25,6 +26,8 @@ def test_cwise_sqrt(context, q, float_data, float_data_gpu):
             continue
         raise Exception('unknown option ', options[i])
         i += 1
+    print('opt_options', opt_options)
+    print('iropencl_options', iropencl_options)
     res = subprocess.run([
         'opt-3.8'
     ] + opt_options + [
@@ -51,31 +54,26 @@ def test_cwise_sqrt(context, q, float_data, float_data_gpu):
 
     N = 10
 
-    # global struct Eigen__TensorEvaluator_7_nopointers* eval_nopointers, global float* eval_ptr0, long eval_ptr0_offset, global float* eval_ptr1, long eval_ptr1_offset, global float* eval_ptr2, long eval_ptr2_offset, long size, local int *scratch
+    # global struct Eigen__TensorEvaluator_nopointers* eval_nopointers, global float* eval_ptr0, long eval_ptr0_offset, global float* eval_ptr1, long eval_ptr1_offset, int size, local int *scratch
 
     # what we need:
-    # struct Eigen__TensorEvaluator_7_nopointers   Note that none of the values we copy across are actually use, so we can just create a sufficiently large buffer...
+    # struct Eigen__TensorEvaluator_nopointers   Note that none of the values we copy across are actually use, so we can just create a sufficiently large buffer...
     # global float *eval_ptr0  => this will receive the result.  just create a sufficiently large buffer
     # ptr0_offset => 0
     # eval_ptr1 => will contian the data we want to reduce
     # eval_ptr1_offset=> 0
-    # eval_ptr2 => just send in a tiny buffer, size 4
-    # eval_ptr2_offset => set to -1, means 'null ointer'
     # size =>  eg 10, to reduce 10 values
     # scratch => set to workgroupsize * sizeof(float)
 
     eval_nopointers_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE, size=4096)
 
-    eval_ptr0 = np.random.randn(1024).astype(np.float32)
+    eval_ptr0 = np.zeros(1024, dtype=np.float32)
     eval_ptr0_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=eval_ptr0)
     eval_ptr0_offset = 0
 
-    eval_ptr1 = np.random.randn(1024).astype(np.float32)
+    eval_ptr1 = np.random.uniform(0, 1, size=(1024,)).astype(np.float32) + 1.0
     eval_ptr1_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.COPY_HOST_PTR, hostbuf=eval_ptr1)
     eval_ptr1_offset = 0
-
-    eval_ptr2_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE, size=128)
-    eval_ptr2_offset = -1
 
     size = N
 
@@ -83,24 +81,21 @@ def test_cwise_sqrt(context, q, float_data, float_data_gpu):
     workgroup_size = 256
     scratch = workgroup_size * 4
 
-    prog.__getattr__('_ZN5Eigen8internal15EigenMetaKernelINS_15TensorEvaluatorIKNS_14TensorAssignOpINS_9TensorMapINS_6TensorIfLi1ELi1EiEELi16ENS_11MakePointerEEEKNS_19TensorCwiseBinaryOpINS0_23scalar_sqrt_gradient_opIfEEKNS4_INS5_IKfLi1ELi1EiEELi16ES7_EEKNS4_INS5_ISC_Li1ELi1ElEELi16ES7_EEEEEENS_9GpuDeviceEEElEEvT_T0_')(
+    prog.__getattr__('_ZN5Eigen8internal15EigenMetaKernelINS_15TensorEvaluatorIKNS_14TensorAssignOpINS_9TensorMapINS_6TensorIfLi1ELi1EiEELi16ENS_11MakePointerEEEKNS_18TensorCwiseUnaryOpINS0_14scalar_sqrt_opIfEEKNS4_INS5_IKfLi1ELi1EiEELi16ES7_EEEEEENS_9GpuDeviceEEEiEEvT_T0_')(
         q, (global_size,), (workgroup_size,),
         eval_nopointers_gpu,
         eval_ptr0_gpu, np.int64(eval_ptr0_offset),
         eval_ptr1_gpu, np.int64(eval_ptr1_offset),
-        eval_ptr2_gpu, np.int64(eval_ptr2_offset),
-        np.int64(size),
+        np.int32(size),
         cl.LocalMemory(scratch)
     )
+    # check for errors
+    q.finish()
     # copy eval_ptr0 back, and check the results...
     cl.enqueue_copy(q, eval_ptr0, eval_ptr0_gpu)
     q.finish()
-    print('eval_ptr0', eval_ptr0[0])
+    print('eval_ptr0[:N]', eval_ptr0[:N])
 
-    assert eval_ptr0[0] != 0
-
-    expected_sum = 0
-    for i in range(N):
-        expected_sum += eval_ptr1[i]
-    print('epxected_sum', expected_sum)
-    assert abs(expected_sum - eval_ptr0[0]) < 1e-4
+    expected = np.sqrt(eval_ptr1)
+    print('expected[:10]', expected[:N])
+    assert np.abs(expected[:N] - eval_ptr0[:N]).max() < 1e-4
