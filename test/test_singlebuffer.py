a2b14f7eab2a1c5729f2647faa8d4a62d3626877
Hugh Perkins <hughperkins@gmail.com>
2016-11-08 13:08:52 +0000

    produce fairly minimal opencl kernel that fails on beignet

--- test/test_singlebuffer.py
+++ test/test_singlebuffer.py
@@ -17,7 +17,7 @@ import pyopencl as cl
 from test import test_common
 
 
-def test_singlebuffer_sqrt_opencl(context, queue):
+def test_singlebuffer_sqrt_opencl_1(context, queue):
     """
     Test doing stuff with one single large buffer for destination and source, just offset a bit
     """
@@ -49,6 +49,56 @@ kernel void myKernel(global float *data0, long offset0, global float *data1, lon
     queue.finish()
 
     prog.myKernel(
+        queue, (global_size,), (workgroup_size,),
+        huge_buf_gpu, np.int64(dst_offset),
+        huge_buf_gpu, np.int64(src_offset),
+        np.int32(N)
+    )
+    queue.finish()
+
+    test_common.enqueue_read_buffer_ext(cl, queue, huge_buf_gpu, dst_host, device_offset=dst_offset, size=N * 4)
+    queue.finish()
+
+    print('src_host', src_host)
+    print('dst_host', dst_host)
+    print('np.sqrt(src_host)', np.sqrt(src_host))
+
+    assert np.abs(np.sqrt(src_host) - dst_host).max() <= 1e-4
+
+
+def test_singlebuffer_sqrt_opencl_2(context, queue):
+    """
+    Test doing stuff with one single large buffer for destination and source, just offset a bit
+    """
+    code = """
+kernel void _Z8myKernelPfS_i(global float* data0, long data0_offset, global float* data1, long data1_offset, int N) {
+    data1 = (global float*)((global char *)data1 + data1_offset);
+    data0 = (global float*)((global char *)data0 + data0_offset);
+
+    if(get_local_id(0) < N) {
+        (&data0[get_local_id(0)])[0] = (float)sqrt((&data1[get_local_id(0)])[0]);
+    }
+}
+"""
+    prog = cl.Program(context, code).build()
+
+    N = 10
+
+    src_host = np.random.uniform(0, 1, size=(N,)).astype(np.float32) + 1.0
+    dst_host = np.zeros(N, dtype=np.float32)
+
+    src_offset = 128
+    dst_offset = 256
+
+    huge_buf_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE, size=4096)
+
+    global_size = 256
+    workgroup_size = 256
+
+    test_common.enqueue_write_buffer_ext(cl, queue, huge_buf_gpu, src_host, device_offset=src_offset, size=N * 4)
+    queue.finish()
+
+    prog._Z8myKernelPfS_i(
         queue, (global_size,), (workgroup_size,),
         huge_buf_gpu, np.int64(dst_offset),
         huge_buf_gpu, np.int64(src_offset),
@@ -114,49 +164,49 @@ __global__ void myKernel(float *data0, float *data1, int N) {
     assert np.abs(np.sqrt(src_host) - dst_host).max() <= 1e-4
 
 
-def test_singlebuffer_hostmapped_sqrt(context, queue):
-    """
-    Test doing stuff with one single large buffer for destination and source, just offset a bit
-    """
-    code = """
-__global__ void myKernel(float *data0, float *data1, int N) {
-    if(threadIdx.x < N) {
-        data0[threadIdx.x] = sqrt(data1[threadIdx.x]);
-    }
-}
-"""
-    prog = test_common.compile_code(cl, context, code)
+# def test_singlebuffer_hostmapped_sqrt(context, queue):
+#     """
+#     Test doing stuff with one single large buffer for destination and source, just offset a bit
+#     """
+#     code = """
+# __global__ void myKernel(float *data0, float *data1, int N) {
+#     if(threadIdx.x < N) {
+#         data0[threadIdx.x] = sqrt(data1[threadIdx.x]);
+#     }
+# }
+# """
+#     prog = test_common.compile_code(cl, context, code)
 
-    N = 10
+#     N = 10
 
-    # src_host = np.random.uniform(0, 1, size=(N,)).astype(np.float32) + 1.0
-    # dst_host = np.zeros(N, dtype=np.float32)
+#     # src_host = np.random.uniform(0, 1, size=(N,)).astype(np.float32) + 1.0
+#     # dst_host = np.zeros(N, dtype=np.float32)
 
-    src_offset = 128
-    dst_offset = 256
+#     src_offset = 128
+#     dst_offset = 256
 
-    huge_buf_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=4096)
+#     huge_buf_gpu = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=4096)
 
-    # test_common.enqueue_write_buffer_ext(cl, queue, huge_buf_gpu, src_host, device_offset=src_offset, size=N * 4)
+#     # test_common.enqueue_write_buffer_ext(cl, queue, huge_buf_gpu, src_host, device_offset=src_offset, size=N * 4)
 
-    global_size = 256
-    workgroup_size = 256
-    # scratch = workgroup_size * 4
+#     global_size = 256
+#     workgroup_size = 256
+#     # scratch = workgroup_size * 4
 
-    mangledName = '_Z8myKernelPfS_i'
-    prog.__getattr__(mangledName)(
-        queue, (global_size,), (workgroup_size,),
-        huge_buf_gpu, np.int64(dst_offset),
-        huge_buf_gpu, np.int64(src_offset),
-        np.int32(N),
-        cl.LocalMemory(4)
-    )
-    queue.finish()
-    # test_common.enqueue_read_buffer_ext(cl, queue, huge_buf_gpu, dst_host, device_offset=dst_offset, size=N * 4)
+#     mangledName = '_Z8myKernelPfS_i'
+#     prog.__getattr__(mangledName)(
+#         queue, (global_size,), (workgroup_size,),
+#         huge_buf_gpu, np.int64(dst_offset),
+#         huge_buf_gpu, np.int64(src_offset),
+#         np.int32(N),
+#         cl.LocalMemory(4)
+#     )
+#     queue.finish()
+#     # test_common.enqueue_read_buffer_ext(cl, queue, huge_buf_gpu, dst_host, device_offset=dst_offset, size=N * 4)
 
-    queue.finish()
-    print('src_host', src_host)
-    print('dst_host', dst_host)
-    print('np.sqrt(src_host)', np.sqrt(src_host))
+#     queue.finish()
+#     print('src_host', src_host)
+#     print('dst_host', dst_host)
+#     print('np.sqrt(src_host)', np.sqrt(src_host))
 
-    assert np.abs(np.sqrt(src_host) - dst_host).max() <= 1e-4
+#     assert np.abs(np.sqrt(src_host) - dst_host).max() <= 1e-4
