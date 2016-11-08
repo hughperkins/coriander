00bc977f26bfd9a345b6e90bdf513f477415fe40
Hugh Perkins <hughperkins@gmail.com>
2016-11-08 00:46:21 +0000

    make cmake handle test options

--- cmake/cocl.cmake
+++ cmake/cocl.cmake
@@ -6,12 +6,12 @@ macro(add_cocl_executable OUTPUT SRCFILE)
     # message("have ${TESTS_COCL_OPTIONS}")
     add_custom_command(
         OUTPUT ${OUTPUT}
-        COMMENT "${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT} ${TESTS_COCL_OPTIONS}"
-        COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT} ${TESTS_COCL_OPTIONS}
+        COMMENT "${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT} ${_TESTS_COCL_OPTIONS}"
+        COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/bin/cocl ${SRCFILE} -o ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT} ${_TESTS_COCL_OPTIONS}
         DEPENDS cocl clblast clew easycl ir-to-opencl patch-hostside
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )
-    add_custom_target(build_${OUTPUT} ALL
+    add_custom_target(build_${OUTPUT}
         DEPENDS ${OUTPUT}
     )
 endmacro(add_cocl_executable)
