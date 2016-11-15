b62537b53682b560e75d7adfa951960e2caad34d
Hugh Perkins <hughperkins@gmail.com>
2016-11-15 12:09:07 +0000

    remove python stuff from docker bulids

--- docker/docker-unittest.sh
+++ docker/docker-unittest.sh
@@ -21,6 +21,19 @@ cocl cuda_sample.cu
 cd /cuda-on-cl/build
 make run-tests
 
+# python stuff...
+# lets add python3 virtualenv, so can run the python tests too
+# this is not strictly necessary for using/running cuda-on-cl
+# in fact, it's not necessary at all :-P
+apt-get install -y --no-install-recommends python3 python3-dev python-virtualenv \
+    python3-numpy python3-wheel python3-setuptools \
+    libffi-dev opencl-headers ocl-icd-opencl-dev
+
+python -m virtualenv -p python3 /env3
+
+. /env3/bin/activate
+pip install numpy
+pip install -r /cuda-on-cl/test/requirements.txt
+
 cd /cuda-on-cl
-source /env3/bin/activate
 pytest -v
