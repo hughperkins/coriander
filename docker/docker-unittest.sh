83ccaabfff766c115faa61f5a03aed887f8f0d83
Hugh Perkins <hughperkins@gmail.com>
2016-11-15 12:43:29 +0000

    comment out py tests in dockre unittest script

--- docker/docker-unittest.sh
+++ docker/docker-unittest.sh
@@ -19,15 +19,17 @@ cocl cuda_sample.cu
 ./cuda_sample
 
 # python stuff...
-apt-get install -y --no-install-recommends python3 python3-dev python-virtualenv \
-    python3-numpy python3-wheel python3 python3-setuptools \
-    python3-mako python3-pip python3-pytest \
-    libffi-dev opencl-headers ocl-icd-opencl-dev
+# apt-get install -y --no-install-recommends python3 python3-dev python-virtualenv \
+#     python3-numpy python3-wheel python3 python3-setuptools \
+#     python3-mako python3-pip python3-pytest \
+#     libffi-dev opencl-headers ocl-icd-opencl-dev
 
-pip3 install pyopencl pytest-pep8
+# pip3 install pyopencl pytest-pep8
 
-cd /cuda-on-cl
-OFFSET_32BIT=1 py.test-3 -v test
+# cd /cuda-on-cl
+# OFFSET_32BIT=1 py.test-3 -v test
 
-cd /cuda-on-cl/build
-make run-tests
+# cd /cuda-on-cl/build
+# make run-tests
+
+# hmmm, the nvidia docker is running ubuntu 14.04 :-P
