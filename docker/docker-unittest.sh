48c50f138b48e3cbb99da3a1ab67122fa134b173
Hugh Perkins <hughperkins@gmail.com>
2016-11-15 10:50:12 +0000

    create a unittest file to run inside the docker images

---
+++ docker/docker-unittest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# I will simply run this, inside the docker image, like:
+
+# docker run -t cocl bash /cuda-on-cl/docker/docker-unittest.sh
+
+# (at least, thats the plan :-) )
+
+set -x
+set -e
+
+cd /cuda-on-cl/build
+./cocl_unittests
+
+mkdir /tmp/foo
+cd /tmp/foo
+cp /cuda-on-cl/test/cocl/cuda_sample.cu .
+cocl cuda_sample.cu
+./cuda_sample
+
+cd /cuda-on-cl/build
+make run-tests
