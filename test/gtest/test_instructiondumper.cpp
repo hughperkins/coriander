2dfb915c1ed0d1668034bb4cd28f268a32b22872
Hugh Perkins <hughperkins@gmail.com>
2016-11-18 23:07:06 +0000

    stash apply

--- test/gtest/test_instructiondumper.cpp
+++ test/gtest/test_instructiondumper.cpp
@@ -39,6 +39,96 @@ namespace {
 LLVMContext context;
 unique_ptr<Module>M;
 
+class InstructionDumperWrapper {
+public:
+    InstructionDumperWrapper() {
+        typeDumper.reset(new TypeDumper(&globalNames));
+        instructionDumper.reset(new InstructionDumper(&globalNames, &localNames, typeDumper.get(), &functionNamesMap,
+            &allocaDeclarations, &variablesToDeclare, &sharedVariablesToDeclare, &shimFunctionsNeeded,
+            &neededFunctions,
+            &globalExpressionByValue, &localExpressionByValue));
+    }
+    virtual ~InstructionDumperWrapper() {
+
+    }
+    void runRhsGeneration(Instruction *inst) {
+        instructionDumper->runRhsGeneration(inst, &extraInstructions);
+    }
+    string getExpr(Instruction *inst) {
+        return instructionDumper->localExpressionByValue->operator[](inst);
+    }
+
+    GlobalNames globalNames;
+    LocalNames localNames;
+    unique_ptr<TypeDumper> typeDumper;
+    FunctionNamesMap functionNamesMap;
+
+    std::vector<AllocaInfo> allocaDeclarations;
+    std::set<llvm::Value *> variablesToDeclare;
+    std::set<llvm::Value *> sharedVariablesToDeclare;
+    std::set<std::string> shimFunctionsNeeded;
+    std::set<llvm::Function *> neededFunctions;
+
+    std::map<llvm::Value *, std::string> globalExpressionByValue;
+    std::map<llvm::Value *, std::string> localExpressionByValue;
+
+    vector<string> extraInstructions;
+
+    unique_ptr<InstructionDumper> instructionDumper;
+};
+
+// Lets think about how instructiondumper should behave for various inputs:
+//
+// we give it an add, for two declared values, it should output eg 'v1 + v2'
+TEST(test_instructiondumper, add_two_declared_variables) {
+    // we should create allocas really, and load those.  I guess?
+    AllocaInst *a = new AllocaInst(IntegerType::get(context, 32));
+    AllocaInst *b = new AllocaInst(IntegerType::get(context, 32));
+
+    LoadInst *aLoad = new LoadInst(a);
+    LoadInst *bLoad = new LoadInst(b);
+
+    InstructionDumperWrapper wrapper;
+
+    // since they are declared, we expect to find them in localnames:
+    wrapper.localNames.getOrCreateName(aLoad, "v_a");
+    wrapper.localNames.getOrCreateName(bLoad, "v_b");
+
+    Instruction *add = BinaryOperator::Create(Instruction::FAdd, aLoad, bLoad);
+
+    wrapper.runRhsGeneration(add);
+    string expr = wrapper.getExpr(add);
+    cout << "expr " << expr << endl;
+    ASSERT_EQ("v_a + v_b", expr);
+
+    // if we check local names, we should NOT find the add, since we havent declared it
+    ASSERT_FALSE(wrapper.localNames.hasValue(add));
+
+    // but we should find an expression for it:
+    /// oh ... we already tested this :-)
+
+    // we should not find a requirement to declare the variable
+    ASSERT_EQ(0u, wrapper.variablesToDeclare.size());
+    // ... and no allocas
+    ASSERT_EQ(0u, wrapper.allocaDeclarations.size());
+}
+
+TEST(test_instructiondumper, callsomething) {
+    Function *childF = cast<Function>(M->getOrInsertFunction(
+        "mychildfunc",
+        PointerType::get(IntegerType::get(context, 8), 0),
+        PointerType::get(IntegerType::get(context, 8), 0),
+        NULL));
+    Value *charArray = new AllocaInst(IntegerType::get(context, 8));
+    CallInst *call = CallInst::Create(childF, charArray);
+
+    InstructionDumperWrapper wrapper;
+    wrapper.localNames.getOrCreateName(charArray, "myCharArray");
+    wrapper.runRhsGeneration(call);
+    string expr = wrapper.getExpr(call);
+    cout << "expr " << expr << endl;
+}
+
 TEST(test_instructiondumper, basic) {
     Value *a = ConstantInt::getSigned(IntegerType::get(context, 32), 123);
     Value *b = ConstantInt::getSigned(IntegerType::get(context, 32), 47);
