bddf0d7c97e6a0043afa8286df4da776526a7344
Hugh Perkins <hughperkins@gmail.com>
2016-11-20 14:47:24 +0000

    remove most of dumpalloca in newinstructiondumper.cpp

--- src/new_instruction_dumper.cpp
+++ src/new_instruction_dumper.cpp
@@ -73,72 +73,75 @@ void NewInstructionDumper::dumpAlloca(cocl::LocalValueInfo *localValueInfo) {
     AllocaInst *alloca = cast<AllocaInst>(localValueInfo->value);
     AllocaInfo allocaInfo;
     localValueInfo->clWriter.reset(new AllocaClWriter(localValueInfo));
-    if(PointerType *allocatypeptr = dyn_cast<PointerType>(alloca->getType())) {
-        Type *ptrElementType = allocatypeptr->getPointerElementType();
-        std::string typestring = typeDumper->dumpType(ptrElementType);
-        int count = readInt32Constant(alloca->getOperand(0));
-        // string name = localNames->getOrCreateName(alloca);
-        string name = localValueInfo->name;
-        // cout << "alloca var name [" << name << "]" << endl;
-        // localExpressionByValue->operator[](alloca) = name;
-        localValueInfo->setExpression(name);
-        if(count == 1) {
-            if(ArrayType *arrayType = dyn_cast<ArrayType>(ptrElementType)) {
-                cout << "alloca, is arraytype" << endl;
-                int innercount = arrayType->getNumElements();
-                Type *elementType = arrayType->getElementType();
-                string allocaDeclaration = typeDumper->dumpType(elementType) + " " + 
-                    localValueInfo->name + "[" + easycl::toString(innercount) + "]";
-                allocaInfo.alloca = alloca;
-                allocaInfo.refValue = alloca;
-                allocaInfo.definition = allocaDeclaration;
-                allocaDeclarations->push_back(allocaInfo);
-                return;
-            } else {
-                cout << "alloca, non-arraytype" << endl;
-                Value *refInstruction = alloca;
-                // if the elementType is a pointer, assume its global?
-                if(isa<PointerType>(ptrElementType)) {
-                    cout << "alloca, pointertype" << endl;
-                    // find the store
-                    for(auto it=alloca->user_begin(); it != alloca->user_end(); it++) {
-                        User *user = *it;
-                        if(StoreInst *store = dyn_cast<StoreInst>(user)) {
-                            int storeop0space = cast<PointerType>(store->getOperand(0)->getType())->getAddressSpace();
-                            // refInstruction = store->getOperand(0);
-                            if(storeop0space == 1) {
-                                gencode += "global ";
-                                updateAddressSpace(alloca, 1);
-                            }
-                            copyAddressSpace(user, alloca);
-                            typestring = typeDumper->dumpType(ptrElementType);
-                        }
-                    }
-                }
-                string allocaDeclaration = gencode + typestring + " " + localValueInfo->name + "[1]";
-                // find the store
-                for(auto it=alloca->user_begin(); it != alloca->user_end(); it++) {
-                    User *user = *it;
-                    if(StoreInst *store = dyn_cast<StoreInst>(user)) {
-                        cout << "found store:" << endl;
-                        store->dump();
-                        cout << endl;
-                        refInstruction = store->getOperand(0);
-                    }
-                }
-                allocaInfo.alloca = alloca;
-                allocaInfo.refValue = refInstruction;
-                allocaInfo.definition = allocaDeclaration;
-                allocaDeclarations->push_back(allocaInfo);
-                return;
-            }
-        } else {
-            throw runtime_error("not implemented: alloca for count != 1");
-        }
-    } else {
-        alloca->dump();
-        throw runtime_error("dumpalloca not implemented for non pointer type");
-    }
+    string name = localValueInfo->name;
+    localValueInfo->setExpression(name);
+
+    // if(PointerType *allocatypeptr = dyn_cast<PointerType>(alloca->getType())) {
+    //     Type *ptrElementType = allocatypeptr->getPointerElementType();
+    //     std::string typestring = typeDumper->dumpType(ptrElementType);
+    //     int count = readInt32Constant(alloca->getOperand(0));
+    //     // string name = localNames->getOrCreateName(alloca);
+    //     string name = localValueInfo->name;
+    //     // cout << "alloca var name [" << name << "]" << endl;
+    //     // localExpressionByValue->operator[](alloca) = name;
+    //     localValueInfo->setExpression(name);
+    //     if(count == 1) {
+    //         if(ArrayType *arrayType = dyn_cast<ArrayType>(ptrElementType)) {
+    //             cout << "alloca, is arraytype" << endl;
+    //             int innercount = arrayType->getNumElements();
+    //             Type *elementType = arrayType->getElementType();
+    //             string allocaDeclaration = typeDumper->dumpType(elementType) + " " + 
+    //                 localValueInfo->name + "[" + easycl::toString(innercount) + "]";
+    //             allocaInfo.alloca = alloca;
+    //             allocaInfo.refValue = alloca;
+    //             allocaInfo.definition = allocaDeclaration;
+    //             allocaDeclarations->push_back(allocaInfo);
+    //             return;
+    //         } else {
+    //             cout << "alloca, non-arraytype" << endl;
+    //             Value *refInstruction = alloca;
+    //             // if the elementType is a pointer, assume its global?
+    //             if(isa<PointerType>(ptrElementType)) {
+    //                 cout << "alloca, pointertype" << endl;
+    //                 // find the store
+    //                 for(auto it=alloca->user_begin(); it != alloca->user_end(); it++) {
+    //                     User *user = *it;
+    //                     if(StoreInst *store = dyn_cast<StoreInst>(user)) {
+    //                         int storeop0space = cast<PointerType>(store->getOperand(0)->getType())->getAddressSpace();
+    //                         // refInstruction = store->getOperand(0);
+    //                         if(storeop0space == 1) {
+    //                             gencode += "global ";
+    //                             updateAddressSpace(alloca, 1);
+    //                         }
+    //                         copyAddressSpace(user, alloca);
+    //                         typestring = typeDumper->dumpType(ptrElementType);
+    //                     }
+    //                 }
+    //             }
+    //             string allocaDeclaration = gencode + typestring + " " + localValueInfo->name + "[1]";
+    //             // find the store
+    //             for(auto it=alloca->user_begin(); it != alloca->user_end(); it++) {
+    //                 User *user = *it;
+    //                 if(StoreInst *store = dyn_cast<StoreInst>(user)) {
+    //                     cout << "found store:" << endl;
+    //                     store->dump();
+    //                     cout << endl;
+    //                     refInstruction = store->getOperand(0);
+    //                 }
+    //             }
+    //             allocaInfo.alloca = alloca;
+    //             allocaInfo.refValue = refInstruction;
+    //             allocaInfo.definition = allocaDeclaration;
+    //             allocaDeclarations->push_back(allocaInfo);
+    //             return;
+    //         }
+    //     } else {
+    //         throw runtime_error("not implemented: alloca for count != 1");
+    //     }
+    // } else {
+    //     alloca->dump();
+    //     throw runtime_error("dumpalloca not implemented for non pointer type");
+    // }
 }
 
 void NewInstructionDumper::dumpBinaryOperator(LocalValueInfo *localValueInfo, std::string opstring) {
