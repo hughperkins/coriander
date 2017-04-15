eb7e6e54ad7523bf0cf2a2c5ca3a72c6523a4f0a
Hugh Perkins <hughperkins@gmail.com>
2017-04-15 13:17:48 +0100

    added unit test for gpu relu forward

--- test/gtest/test_dnn_act.cpp
+++ test/gtest/test_dnn_act.cpp
@@ -148,155 +148,139 @@ TEST(test_dnn_act, backward_cpu) {
     delete[] gradInput;
 }
 
-// TEST(test_dnn_act, gpu_forward) {
-//     int N = 4;
-//     int C = 3;
-//     int inH = 5;
-//     int inW = 6;
-//     int kH = 3;
-//     int kW = 3;
-//     int padH = 1;
-//     int padW = 1;
-//     int dH = 1;
-//     int dW = 1;
+TEST(test_dnn_act, gpu_forward_relu) {
+    int N = 4;
+    int C = 3;
+    int H = 5;
+    int W = 6;
 
-//     // N = 1;
-//     // C = 1;
-//     // inH = 3;
-//     // inW = 3;
+    // N = 1;
+    // C = 1;
+    // inH = 3;
+    // inW = 3;
 
-//     int outH = (inH + 2 * padH - kH) / dH + 1;
-//     int outW = (inW + 2 * padW - kW) / dW + 1;
-//     cout << "outH=" << outH << " outW=" << outW << endl;
+    int linearSize = N * C * H * W;
 
-//     int inLinearSize = N * C * inH * inW;
-//     int outLinearSize = N * C * outH * outW;
+    float *input = new float[linearSize];
+    float *output = new float[linearSize];
 
-//     float *input = new float[inLinearSize];
-//     float *output = new float[outLinearSize];
+    MT19937 random;
+    random.seed(123ul);
 
-//     MT19937 random;
-//     random.seed(123ul);
+    fillRandomUniform(random, input, N * C * H * W, -1.0f, 1.0f);
 
-//     fillRandomUniform(random, input, N * C * inH * inW, 0.0f, 1.0f);
+    forward_relu_cpu(input, N, C, H, W, output);
 
-//     pool_forward_cpu(input, N, C, inH, inW, kH, kW, padH, padW, dH, dW, output);
+    cout << "input[0][0]:" << endl;
+    for(int h=0; h < H; h++) {
+        for(int w=0; w < W; w++) {
+            cout << input[h * W + w] << " ";
+        }
+        cout << endl;
+    }
 
-//     cout << "input[0][0]:" << endl;
-//     for(int h=0; h < inH; h++) {
-//         for(int w=0; w < inW; w++) {
-//             cout << input[h * inW + w] << " ";
-//         }
-//         cout << endl;
-//     }
+    cout << "output[0][0]:" << endl;
+    for(int h=0; h < H; h++) {
+        for(int w=0; w < W; w++) {
+            cout << output[h * W + w] << " ";
+        }
+        cout << endl;
+    }
 
-//     cout << "output[0][0]:" << endl;
-//     for(int h=0; h < outH; h++) {
-//         for(int w=0; w < outW; w++) {
-//             cout << output[h * outW + w] << " ";
-//         }
-//         cout << endl;
-//     }
+    cudnnHandle_t dnn_handle;
+    cudnnTensorDescriptor_t inputDesc;
+    cudnnTensorDescriptor_t outputDesc;
+    cudnnActivationDescriptor_t actDesc;
 
-//     cudnnHandle_t dnn_handle;
-//     cudnnTensorDescriptor_t inputDesc;
-//     cudnnTensorDescriptor_t outputDesc;
-//     cudnnPoolingDescriptor_t poolDesc;
+    cudnnCreate(&dnn_handle);
+    cudnnCreateTensorDescriptor(&inputDesc);
+    cudnnCreateTensorDescriptor(&outputDesc);
+    cudnnCreateActivationDescriptor(&actDesc);
 
-//     cudnnCreate(&dnn_handle);
-//     cudnnCreateTensorDescriptor(&inputDesc);
-//     cudnnCreateTensorDescriptor(&outputDesc);
-//     cudnnCreatePoolingDescriptor(&poolDesc);
+    cudnnSetTensor4dDescriptor(
+        inputDesc,
+        CUDNN_TENSOR_NCHW,
+        CUDNN_DATA_FLOAT,
+        N, C, H, W);
+    cudnnSetTensor4dDescriptor(
+        outputDesc,
+        CUDNN_TENSOR_NCHW,
+        CUDNN_DATA_FLOAT,
+        N, C, H, W);
+    cudnnSetActivationDescriptor(actDesc,
+                                 CUDNN_ACTIVATION_RELU,
+                                 CUDNN_PROPAGATE_NAN, 0.0);
 
-//     cudnnSetTensor4dDescriptor(
-//         inputDesc,
-//         CUDNN_TENSOR_NCHW,
-//         CUDNN_DATA_FLOAT,
-//         N, C, inH, inW);
-//     cudnnSetTensor4dDescriptor(
-//         outputDesc,
-//         CUDNN_TENSOR_NCHW,
-//         CUDNN_DATA_FLOAT,
-//         N, C, outH, outW);
-//     cudnnSetPooling2dDescriptor(poolDesc,
-//                                 CUDNN_POOLING_MAX,
-//                                 CUDNN_PROPAGATE_NAN,
-//                                 kH, kW,
-//                                 padH, padW,
-//                                 dH, dW);
+    ThreadVars *v = getThreadVars();
+    EasyCL *cl = v->getContext()->getCl();
 
-//     ThreadVars *v = getThreadVars();
-//     EasyCL *cl = v->getContext()->getCl();
+    size_t inputOffsetBytes = 0;
+    size_t outputOffsetBytes = inputOffsetBytes + linearSize * sizeof(float);
+    size_t gpuMemoryAllocSize = outputOffsetBytes + linearSize * sizeof(float);
 
-//     size_t inputOffsetBytes = 0;
-//     size_t outputOffsetBytes = inputOffsetBytes + inLinearSize * sizeof(float);
-//     size_t gpuMemoryAllocSize = outputOffsetBytes + outLinearSize * sizeof(float);
+    cout << "gpuMemoryAllocSize=" << gpuMemoryAllocSize << endl;
+    Memory *gpuMemory = Memory::newDeviceAlloc(gpuMemoryAllocSize);
 
-//     cout << "gpuMemoryAllocSize=" << gpuMemoryAllocSize << endl;
-//     Memory *gpuMemory = Memory::newDeviceAlloc(gpuMemoryAllocSize);
+    float *gpuDeviceInput = (float *)(((char *)gpuMemory->fakePos + inputOffsetBytes));
+    float *gpuDeviceOutput = (float *)(((char *)gpuMemory->fakePos + outputOffsetBytes));
 
-//     float *gpuDeviceInput = (float *)(((char *)gpuMemory->fakePos + inputOffsetBytes));
-//     float *gpuDeviceOutput = (float *)(((char *)gpuMemory->fakePos + outputOffsetBytes));
+    cl_int err;
 
-//     cl_int err;
+    err = clEnqueueWriteBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, inputOffsetBytes,
+                                     (linearSize) * sizeof(float), input, 0, NULL, NULL);
+    EasyCL::checkError(err);
 
-//     err = clEnqueueWriteBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, inputOffsetBytes,
-//                                      (inLinearSize) * sizeof(float), input, 0, NULL, NULL);
-//     EasyCL::checkError(err);
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    cudnnActivationForward(dnn_handle, actDesc, &alpha,
+                           inputDesc, gpuDeviceInput,
+                           &beta,
+                           outputDesc, gpuDeviceOutput);
 
-//     float alpha = 1.0f;
-//     float beta = 0.0f;
-//     cudnnPoolingForward(dnn_handle,
-//                         poolDesc,
-//                         &alpha,
-//                         inputDesc, gpuDeviceInput,  // input to this layer
-//                         &beta,
-//                         outputDesc, gpuDeviceOutput);  // output
+    float *gpuOutHostside = new float[linearSize];
+    err = clEnqueueReadBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, outputOffsetBytes,
+                                     (linearSize) * sizeof(float), gpuOutHostside, 0, NULL, NULL);
+    EasyCL::checkError(err);
+    cl->finish();
 
-//     float *gpuOutHostside = new float[outLinearSize];
-//     err = clEnqueueReadBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, outputOffsetBytes,
-//                                      (outLinearSize) * sizeof(float), gpuOutHostside, 0, NULL, NULL);
-//     EasyCL::checkError(err);
-//     cl->finish();
+    cout << "gpuOutput[0][0]:" << endl;
+    for(int h=0; h < H; h++) {
+        for(int w=0; w < W; w++) {
+            cout << gpuOutHostside[h * W + w] << " ";
+        }
+        cout << endl;
+    }
 
-//     cout << "gpuOutput[0][0]:" << endl;
-//     for(int h=0; h < outH; h++) {
-//         for(int w=0; w < outW; w++) {
-//             cout << gpuOutHostside[h * outW + w] << " ";
-//         }
-//         cout << endl;
-//     }
+    const int numSamples = 20;
+    int *sampleIndices = new int[numSamples];
+    fillRandomInt(random, sampleIndices, numSamples, 0, linearSize);
+    for(int i = 0; i < numSamples; i++) {
+        int linearPos = sampleIndices[i];
+        int n = linearPos / C / H / W;
+        int rem = linearPos - n * C * H * W;
+        int c = rem / H / W;
+        rem = rem - c * H * W;
+        int outh = rem / W;
+        int outw = rem % W;
+        cout << "n=" << n << " c=" << c << " outh=" << outh << " outw=" << outw << " output[" << linearPos << "]="
+            << output[linearPos] << " " << gpuOutHostside[linearPos] << endl;
+        if(abs(output[linearPos] - gpuOutHostside[linearPos]) > 1e-4) {
+            throw runtime_error(string("test_dnn, output of conv forward ,mismatch for ") +
+                "n=" + toString(n) + " c=" + toString(c) + " outh=" + toString(outh) + " outw=" + toString(outw));
+        }
+    }
 
-//     const int numSamples = 20;
-//     int *sampleIndices = new int[numSamples];
-//     fillRandomInt(random, sampleIndices, numSamples, 0, outLinearSize);
-//     for(int i = 0; i < numSamples; i++) {
-//         int linearPos = sampleIndices[i];
-//         int n = linearPos / C / outH / outW;
-//         int rem = linearPos - n * C * outH * outW;
-//         int c = rem / outH / outW;
-//         rem = rem - c * outH * outW;
-//         int outh = rem / outW;
-//         int outw = rem % outW;
-//         cout << "n=" << n << " c=" << c << " outh=" << outh << " outw=" << outw << " output[" << linearPos << "]="
-//             << output[linearPos] << " " << gpuOutHostside[linearPos] << endl;
-//         if(abs(output[linearPos] - gpuOutHostside[linearPos]) > 1e-4) {
-//             throw runtime_error(string("test_dnn, output of conv forward ,mismatch for ") +
-//                 "n=" + toString(n) + " c=" + toString(c) + " outh=" + toString(outh) + " outw=" + toString(outw));
-//         }
-//     }
+    cudnnDestroyActivationDescriptor(actDesc);
+    cudnnDestroyTensorDescriptor(inputDesc);
+    cudnnDestroyTensorDescriptor(outputDesc);
+    cudnnDestroy(dnn_handle);
 
-//     cudnnDestroyPoolingDescriptor(poolDesc);
-//     cudnnDestroyTensorDescriptor(inputDesc);
-//     cudnnDestroyTensorDescriptor(outputDesc);
-//     cudnnDestroy(dnn_handle);
+    delete gpuMemory;
+    delete[] gpuOutHostside;
 
-//     delete gpuMemory;
-//     delete[] gpuOutHostside;
-
-//     delete[] output;
-//     delete[] input;
-// }
+    delete[] output;
+    delete[] input;
+}
 
 // TEST(test_dnn_act, gpu_backward) {
 //     int N = 4;
