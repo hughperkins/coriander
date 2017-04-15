f85e784e0abee99625080665750283aa85cfc459
Hugh Perkins <hughperkins@gmail.com>
2017-04-15 13:00:55 +0200

    create test for pooling backward gpu

--- test/gtest/test_dnn_pooling.cpp
+++ test/gtest/test_dnn_pooling.cpp
@@ -447,4 +447,177 @@ TEST(test_dnn_pooling, gpu_forward) {
     delete[] input;
 }
 
+TEST(test_dnn_pooling, gpu_backward) {
+    int N = 4;
+    int C = 3;
+    int inH = 5;
+    int inW = 6;
+    int kH = 3;
+    int kW = 3;
+    int padH = 1;
+    int padW = 1;
+    int dH = 1;
+    int dW = 1;
+
+    // N = 1;
+    // C = 1;
+    // inH = 3;
+    // inW = 3;
+
+    int outH = (inH + 2 * padH - kH) / dH + 1;
+    int outW = (inW + 2 * padW - kW) / dW + 1;
+    cout << "outH=" << outH << " outW=" << outW << endl;
+
+    int inLinearSize = N * C * inH * inW;
+    int outLinearSize = N * C * outH * outW;
+
+    float *input = new float[inLinearSize];
+    float *output = new float[outLinearSize];
+    float *gradInput = new float[inLinearSize];
+
+    MT19937 random;
+    random.seed(123ul);
+
+    fillRandomUniform(random, input, N * C * inH * inW, 0.0f, 1.0f);
+
+    pool_forward_cpu(input, N, C, inH, inW, kH, kW, padH, padW, dH, dW, output);
+    pool_backward_cpu(output, output, input, N, C, inH, inW, kH, kW, padH, padW, dH, dW, gradInput);
+
+    cout << "input[0][0]:" << endl;
+    for(int h=0; h < inH; h++) {
+        for(int w=0; w < inW; w++) {
+            cout << input[h * inW + w] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "output[0][0]:" << endl;
+    for(int h=0; h < outH; h++) {
+        for(int w=0; w < outW; w++) {
+            cout << output[h * outW + w] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "gradInput[0][0]:" << endl;
+    for(int h=0; h < inH; h++) {
+        for(int w=0; w < inW; w++) {
+            cout << gradInput[h * inW + w] << " ";
+        }
+        cout << endl;
+    }
+
+    cudnnHandle_t dnn_handle;
+    cudnnTensorDescriptor_t inputDesc;
+    cudnnTensorDescriptor_t outputDesc;
+    // cudnnTensorDescriptor_t gradInputDesc;
+    cudnnPoolingDescriptor_t poolDesc;
+
+    cudnnCreate(&dnn_handle);
+    cudnnCreateTensorDescriptor(&inputDesc);
+    cudnnCreateTensorDescriptor(&outputDesc);
+    // cudnnCreateTensorDescriptor(&gradInputDesc);
+    cudnnCreatePoolingDescriptor(&poolDesc);
+
+    cudnnSetTensor4dDescriptor(
+        inputDesc,
+        CUDNN_TENSOR_NCHW,
+        CUDNN_DATA_FLOAT,
+        N, C, inH, inW);
+    cudnnSetTensor4dDescriptor(
+        outputDesc,
+        CUDNN_TENSOR_NCHW,
+        CUDNN_DATA_FLOAT,
+        N, C, outH, outW);
+    cudnnSetPooling2dDescriptor(poolDesc,
+                                CUDNN_POOLING_MAX,
+                                CUDNN_PROPAGATE_NAN,
+                                kH, kW,
+                                padH, padW,
+                                dH, dW);
+
+    ThreadVars *v = getThreadVars();
+    EasyCL *cl = v->getContext()->getCl();
+
+    size_t inputOffsetBytes = 0;
+    size_t outputOffsetBytes = inputOffsetBytes + inLinearSize * sizeof(float);
+    size_t gradInputOffsetBytes = outputOffsetBytes + outLinearSize * sizeof(float);
+    size_t gpuMemoryAllocSize = gradInputOffsetBytes + inLinearSize * sizeof(float);
+
+    cout << "gpuMemoryAllocSize=" << gpuMemoryAllocSize << endl;
+    Memory *gpuMemory = Memory::newDeviceAlloc(gpuMemoryAllocSize);
+
+    float *gpuDeviceInput = (float *)(((char *)gpuMemory->fakePos + inputOffsetBytes));
+    float *gpuDeviceOutput = (float *)(((char *)gpuMemory->fakePos + outputOffsetBytes));
+    float *gpuDeviceGradInput = (float *)(((char *)gpuMemory->fakePos + gradInputOffsetBytes));
+
+    cl_int err;
+
+    err = clEnqueueWriteBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, inputOffsetBytes,
+                                     (inLinearSize) * sizeof(float), input, 0, NULL, NULL);
+    EasyCL::checkError(err);
+
+    float alpha = 1.0f;
+    float beta = 0.0f;
+    cudnnPoolingForward(dnn_handle,
+                        poolDesc,
+                        &alpha,
+                        inputDesc, gpuDeviceInput,  // input to this layer
+                        &beta,
+                        outputDesc, gpuDeviceOutput);  // output
+    cudnnPoolingBackward(dnn_handle,
+                        poolDesc,
+                        &alpha,
+                        outputDesc, gpuDeviceOutput,
+                        outputDesc, gpuDeviceOutput,
+                        inputDesc, gpuDeviceInput,
+                        &beta,
+                        inputDesc, gpuDeviceGradInput);
+
+    float *gpuGradInputHostside = new float[inLinearSize];
+    err = clEnqueueReadBuffer(v->currentContext->default_stream.get()->clqueue->queue, gpuMemory->clmem, CL_TRUE, gradInputOffsetBytes,
+                                     (outLinearSize) * sizeof(float), gpuGradInputHostside, 0, NULL, NULL);
+    EasyCL::checkError(err);
+    cl->finish();
+
+    cout << "gpuGradInput[0][0]:" << endl;
+    for(int h=0; h < inH; h++) {
+        for(int w=0; w < inW; w++) {
+            cout << gpuGradInputHostside[h * outW + w] << " ";
+        }
+        cout << endl;
+    }
+
+    const int numSamples = 20;
+    int *sampleIndices = new int[numSamples];
+    fillRandomInt(random, sampleIndices, numSamples, 0, inLinearSize);
+    for(int i = 0; i < numSamples; i++) {
+        int linearPos = sampleIndices[i];
+        int n = linearPos / C / inH / inW;
+        int rem = linearPos - n * C * inH * inW;
+        int c = rem / inH / inW;
+        rem = rem - c * inH * inW;
+        int inh = rem / inW;
+        int inw = rem % inW;
+        cout << "n=" << n << " c=" << c << " inh=" << inh << " inw=" << inw << " gradInput[" << linearPos << "]="
+            << gradInput[linearPos] << " " << gpuGradInputHostside[linearPos] << endl;
+        if(abs(gradInput[linearPos] - gpuGradInputHostside[linearPos]) > 1e-4) {
+            throw runtime_error(string("test_dnn, output of pool backward ,mismatch for ") +
+                "n=" + toString(n) + " c=" + toString(c) + " outh=" + toString(inh) + " outw=" + toString(inw));
+        }
+    }
+
+    cudnnDestroyPoolingDescriptor(poolDesc);
+    cudnnDestroyTensorDescriptor(inputDesc);
+    cudnnDestroyTensorDescriptor(outputDesc);
+    cudnnDestroy(dnn_handle);
+
+    delete gpuMemory;
+    delete[] gpuGradInputHostside;
+
+    delete[] output;
+    delete[] input;
+    delete[] gradInput;
+}
+
 } // namespace
