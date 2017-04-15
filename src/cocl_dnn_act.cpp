14fbafc8d464265a1a13a4bb35e5c04a329b9109
Hugh Perkins <hughperkins@gmail.com>
2017-04-15 13:23:44 +0100

    relu forward implemented, builds ok, runs ok, passes test

--- src/cocl_dnn_act.cpp
+++ src/cocl_dnn_act.cpp
@@ -16,6 +16,24 @@ using namespace std;
 using namespace cocl;
 using namespace cocl::dnn;
 
+static string get_ReluForward_sourcecode();
+
+inline int getNumThreads() {
+  // int blockSize = 1024;
+  // int maxWorkgroupSize = ((easycl::DeviceInfo *)state->deviceInfoByDevice[state->currentDevice])->maxWorkGroupSize;
+  // if( blockSize > maxWorkgroupSize ) {
+  //   blockSize = maxWorkgroupSize;
+  // }
+  // return blockSize;
+    return 256; // just hardcode to 256 for now, which covers amd, intel, nvidia, just not always most efficiently, but
+                // kind of ok
+}
+
+// CL: number of blocks for threads.
+inline int GET_BLOCKS(const int N) {
+  return (N + getNumThreads() - 1) / getNumThreads();
+}
+
 size_t cudnnCreateActivationDescriptor(cudnnActivationDescriptor_t *p_desc) {
     *p_desc = new ActivationDescriptor();
     return 0;
@@ -40,7 +58,40 @@ size_t cudnnActivationForward(
     float *p_beta,
     cudnnTensorDescriptor_t outputDesc, float *outputData
 ) {
-    throw runtime_error("not implemented");
+    ThreadVars *v = getThreadVars();
+    cl_int err;
+
+    Memory *inputMemory = findMemory((const char *)inputData);
+    Memory *outputMemory = findMemory((const char *)outputData);
+
+    size_t inputOffset = inputMemory->getOffset((const char *)inputData);
+    size_t outputOffset = outputMemory->getOffset((const char *)outputData);
+
+    CoclDnnGeometryType N = inputDesc->N;
+    CoclDnnGeometryType C = inputDesc->C;
+    CoclDnnGeometryType H = inputDesc->H;
+    CoclDnnGeometryType W = inputDesc->W;
+
+    easycl::CLKernel *kernel = getKernelForNameCl("ReluForward", get_ReluForward_sourcecode());
+
+    int linearSize = N * C * H * W;
+
+    kernel->in((int)linearSize);
+
+    kernel->inout(&inputMemory->clmem);
+    kernel->in((int32_t)(inputOffset / sizeof(float)));
+
+    kernel->inout(&outputMemory->clmem);
+    kernel->in((int32_t)(outputOffset / sizeof(float)));
+
+    int workgroupSize = getNumThreads();
+    int globalSize = GET_BLOCKS(linearSize) * workgroupSize;
+    kernel->run_1d(&v->currentContext->default_stream.get()->clqueue->queue, globalSize, workgroupSize);
+    // int status = clFinish(v->currentContext->default_stream.get()->clqueue->queue);
+    // if(status != 0) {
+    //     cout << "status" << status << endl;
+    //     throw runtime_error("Pooling returned non-zero status");
+    // }
 }
 size_t cudnnActivationBackward(
     cudnnHandle_t handle,
@@ -53,4 +104,33 @@ size_t cudnnActivationBackward(
     cudnnTensorDescriptor_t gradInputDesc, float *gradInputData
 ) {
     throw runtime_error("not implemented");
+}
+
+string get_ReluForward_sourcecode() {
+    return R"(
+// CL: grid stride looping
+#define CL_KERNEL_LOOP(i, n)                        \
+  for (int i = get_group_id(0) * get_local_size(0) + get_local_id(0); \
+      i < (n);                                       \
+      i += get_local_size(0) * get_num_groups(0))
+
+#define Dtype float
+
+kernel void ReluForward(
+    const int nthreads,
+    global const Dtype* input_data, int input_offset,
+    global Dtype* output_data, int output_offset
+  ) {
+
+  global const Dtype *input = input_data + input_offset;
+  global Dtype *output = output_data + output_offset;
+
+  CL_KERNEL_LOOP(index, nthreads) {
+    if(index < nthreads) {
+        float inval = input[index];
+        output[index] = inval > 0 ? inval : 0.0f;
+    }
+  }
+}
+)";
 }
