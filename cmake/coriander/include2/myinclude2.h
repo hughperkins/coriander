2c6f2bea9ca8cce2fbfe21795902ecc205bdea07
Hugh Perkins <hughperkins@gmail.com>
2017-06-04 17:31:40 +0100

    add test for include_directories

---
+++ cmake/coriander/include2/myinclude2.h
@@ -0,0 +1,3 @@
+#pragma once
+
+// not much here...
--- cmake/coriander/many_cpp.cpp
+++ cmake/coriander/many_cpp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "many_cpp.h"
+#include "myinclude2.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "many_cpp" << std::endl;
