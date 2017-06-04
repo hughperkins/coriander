c0ca483978b339635135f314c0603a152c8e96be
Hugh Perkins <hughperkins@gmail.com>
2017-06-04 16:46:47 +0100

    handle multiple files

---
+++ cmake/coriander/include/many_cpp.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void many_cc();
+void many_cu();
---
+++ cmake/coriander/many_cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "many_cpp.h"
+
+int main(int argc, char *argv[]) {
+    std::cout << "many_cpp" << std::endl;
+    many_cc();
+    many_cu();
+    return 0;
+}
