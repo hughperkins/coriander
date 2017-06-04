ea4522b3b8595b2016122ec860f89b9c5782c112
Hugh Perkins <hughperkins@gmail.com>
2017-06-04 16:28:00 +0100

    playing with better cmake module commands for coriander

---
+++ cmake/coriander/cuda_sample.cu
@@ -0,0 +1,45 @@
+// a cuda app.  we will convert this to opencl, and run it :-)
+
+#include <iostream>
+#include <memory>
+#include <cassert>
+
+using namespace std;
+
+#include <cuda_runtime.h>
+
+__global__ void setValue(float *data, int idx, float value) {
+    if(threadIdx.x == 0) {
+        data[idx] = value;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int N = 1024;
+
+    float *gpuFloats;
+    cudaMalloc((void**)(&gpuFloats), N * sizeof(float));
+
+    setValue<<<dim3(32, 1, 1), dim3(32, 1, 1)>>>(gpuFloats, 2, 123.0f);
+
+    float hostFloats[4];
+    cudaMemcpy(hostFloats, gpuFloats, 4 * sizeof(float), cudaMemcpyDeviceToHost);
+    cout << "hostFloats[2] " << hostFloats[2] << endl;
+    assert(hostFloats[2] == 123.0f);
+
+    setValue<<<dim3(32, 1, 1), dim3(32, 1, 1)>>>(gpuFloats, 2, 222.0f);
+    cudaMemcpy(hostFloats, gpuFloats, 4 * sizeof(float), cudaMemcpyDeviceToHost);
+    cout << "hostFloats[2] " << hostFloats[2] << endl;
+    assert(hostFloats[2] == 222.0f);
+
+    hostFloats[2] = 444.0f;
+    cudaMemcpy(gpuFloats, hostFloats, 4 * sizeof(float), cudaMemcpyHostToDevice);
+    hostFloats[2] = 555.0f;
+    cudaMemcpy(hostFloats, gpuFloats, 4 * sizeof(float), cudaMemcpyDeviceToHost);
+    cout << "hostFloats[2] " << hostFloats[2] << endl;
+    assert(hostFloats[2] == 444.0f);
+
+    cudaFree(gpuFloats);
+
+    return 0;
+}
