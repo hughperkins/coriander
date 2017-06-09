6dd50433880c51b35370224a1b253346a186b548
Hugh Perkins <hughperkins@gmail.com>
2017-06-09 17:43:45 +0100

    headers, remove dead code

--- src/GlobalNames.h
+++ src/GlobalNames.h
@@ -1,3 +1,17 @@
+// Copyright Hugh Perkins 2016, 2017
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include "llvm/IR/DerivedTypes.h"
@@ -8,28 +22,6 @@
 #include <string>
 
 namespace cocl {
-
-// class Nameable {
-// public:
-//     virtual bool isType() { return false }
-//     virtual bool isValue() { return false }
-//     virtual Value *asValue() { throw runtime_error("Not a value") }
-//     virtual Value *asType() { throw runtime_error("Not a type") }
-// };
-
-// class NamedType : Nameable {
-// public:
-//     llvm::Type *type;
-//     virtual bool isType() { return true }
-//     virtual Value *asType() { return type; }
-// };
-
-// class NamedValue : Nameable {
-// public:
-//     Value *value;
-//     virtual bool isValue() { return true }
-//     virtual Value *asValue() { return value; }
-// };
 
 class GlobalNames {
 public:
