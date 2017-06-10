0932100b37d626792da20ca073df6258e1f450c6
Hugh Perkins <hughperkins@gmail.com>
2017-06-10 19:42:53 +0100

    jenkins/u1604.sh

---
+++ jenkins/u1604.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# assumed to run from the base of the cloned Coriander git repo
+
+BASEDIR=$(pwd)
+hg clone https://bitbucket.org/hughperkins/eigen -b tf-coriander
+mkdir build
+cd build
+cmake  -DEIGEN_TESTS=ON -DEIGEN_HOME=${BASEDIR}/eigen ..
+make -j 16
+make -j 16 tests
+make run-tests
+
+# cd docker
+# sudo docker build --no-cache ./ -t coriander
