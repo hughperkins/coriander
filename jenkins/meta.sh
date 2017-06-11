a499de623463ea8c26853c5ed91c291ad9587f57
Hugh Perkins <hughperkins@gmail.com>
2017-06-11 14:48:25 +0100

    sync meta with tf-coriander one

--- jenkins/meta.sh
+++ jenkins/meta.sh
@@ -5,6 +5,9 @@
 
 set -e
 set -x
+set -o pipefail
+
+PROJECT=coriander
 
 if [ -z "${BUILD_NUMBER}" ]; then {
     BUILD_NUMBER=test
@@ -14,43 +17,59 @@ if [ -z "${BUILD_NUMBER}" ]; then {
 GIT_COMMIT_SHORT=$(git log --oneline -n 1 | awk '{print $1}')
 # } fi
 
-(
-set +x
+function update_project_index {
+    echo '<html><body>' > newhtml.html
+    for filename in $(aws s3 ls s3://hughperkins-ci/${PROJECT}/ | grep PRE | awk '{print $2}' | cut -d \. -f 1); do {
+        echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/${PROJECT}/${filename}.d/index.html\">${filename}</a><br />" >> newhtml.html
+    } done
+    echo '</body></html>' >> newhtml.html
+    aws s3 cp newhtml.html s3://hughperkins-ci/${PROJECT}/index.html
+}
 
-BUILD_URL="https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/index.html"
-curl -XPOST -H "Authorization: token ${OAUTH_TOKEN}" https://api.github.com/repos/hughperkins/coriander/statuses/$(git rev-parse HEAD) -d "{
-  \"state\": \"pending\",
-  \"description\": \"Building\"
-}"
-)
+function upload_artifacts {
+    echo '<html><body>' > newhtml.html
 
-bash jenkins/u1604.sh | tee out.log
+    for artifact in $1; do {
+        if [[ -f ${artifact} ]]; then {
+            aws s3 cp ${artifact} s3://hughperkins-ci/${PROJECT}/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-${artifact}
+            echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/${PROJECT}/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-${artifact}\">${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-${artifact}</a><br />" >> newhtml.html
+        } fi
+    } done
 
-echo '<html><body>' > newhtml.html
+    echo '</body></html>' >> newhtml.html
+    aws s3 cp newhtml.html s3://hughperkins-ci/${PROJECT}/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/index.html
+}
 
-aws s3 cp out.log s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-build.log
-echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-build.log\">${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-build.log</a><br />" >> newhtml.html
+function set_status {
+    status_string=$1
+    description="$2"
 
-aws s3 cp build/artifacts.zip s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-artifacts.zip
-echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-artifacts.zip\">${BUILD_NUMBER}-${GIT_COMMIT_SHORT}-artifacts.zip</a><br />" >> newhtml.html
+    BUILD_URL="https://s3.amazonaws.com/hughperkins-ci/${PROJECT}/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/index.html"
+    target_url_str="\"target_url\": \"${BUILD_URL}\","
+    if [[ ${status_string} == pending ]]; then {
+        target_url_str=
+    } fi
+    (
+        set +x
+        curl -XPOST -H "Authorization: token ${OAUTH_TOKEN}" https://api.github.com/repos/hughperkins/${PROJECT}/statuses/$(git rev-parse HEAD) -d "{
+          \"state\": \"${status_string}\",
+          ${target_url_str}
+          \"description\": \"${description}\"
+        }"
+    )
+}
 
-echo '</body></html>' >> newhtml.html
-aws s3 cp newhtml.html s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/index.html
+function error {
+    upload_artifacts build.log
+    update_project_index
+    set_status failure "Build or tests failed"
+    exit 1
+}
 
-echo '<html><body>' > newhtml.html
-for filename in $(aws s3 ls s3://hughperkins-ci/coriander/ | grep PRE | awk '{print $2}' | cut -d \. -f 1); do {
-    echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${filename}.d/index.html\">${filename}</a><br />" >> newhtml.html
-} done
-echo '</body></html>' >> newhtml.html
-aws s3 cp newhtml.html s3://hughperkins-ci/coriander/index.html
+set_status pending Building
 
-(
-set +x
+bash jenkins/u1604.sh 2>&1 | tee build.log || error
 
-BUILD_URL="https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT_SHORT}.d/index.html"
-curl -XPOST -H "Authorization: token ${OAUTH_TOKEN}" https://api.github.com/repos/hughperkins/coriander/statuses/$(git rev-parse HEAD) -d "{
-  \"state\": \"success\",
-  \"target_url\": \"${BUILD_URL}\",
-  \"description\": \"Build and tests completed successfully\"
-}"
-)
+upload_artifacts 'build.log artifacts.zip'
+update_project_index
+set_status success "Build and tests completed successfully"
