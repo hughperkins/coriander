2a90e1f5584467559fe3c05ba6e4ed83fa2c84dc
Hugh Perkins <hughperkins@gmail.com>
2017-06-11 10:04:25 +0100

    upload logs and artifacdts to s3

---
+++ jenkins/meta.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# - handles running u1604.sh, tee'ing to a log file, and uploading this logfile to s3
+# - assumes it runs from root of cloend coriander repo
+
+if [ -z "${BUILD_NUMBER}" ]; then {
+    BUILD_NUMBER=test
+} fi
+
+if [ -z "${GIT_COMMIT}" ]; then {
+    GIT_COMMIT=$(git log --oneline -n 1 | awk '{print $1}')
+} fi
+
+bash jenkins/u1604.sh | tee out.log
+
+echo '<html><body>' > newhtml.html
+
+aws s3 cp out.log s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT}.d/${BUILD_NUMBER}-${GIT_COMMIT}-build.log
+echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT}.d/${BUILD_NUMBER}-${GIT_COMMIT}-build.log\">${BUILD_NUMBER}-${GIT_COMMIT}-build.log</a><br />" >> newhtml.html
+
+aws s3 cp build/artifacts.zip s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT}.d/${BUILD_NUMBER}-${GIT_COMMIT}-artifacts.zip
+echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT}.d/${BUILD_NUMBER}-${GIT_COMMIT}-artifacts.zip\">${BUILD_NUMBER}-${GIT_COMMIT}-artifacts.zip</a><br />" >> newhtml.html
+
+echo '</body></html>' >> newhtml.html
+aws s3 cp newhtml.html s3://hughperkins-ci/coriander/${BUILD_NUMBER}-${GIT_COMMIT}.d/index.html
+
+echo '<html><body>' > newhtml.html
+for filename in $(aws s3 ls s3://hughperkins-ci/coriander/ | grep PRE | awk '{print $2}' | cut -d \. -f 1); do {
+    echo "<a href=\"https://s3.amazonaws.com/hughperkins-ci/coriander/${filename}.d/index.html\">${filename}</a><br />" >> newhtml.html
+} done
+echo '</body></html>' >> newhtml.html
+aws s3 cp newhtml.html s3://hughperkins-ci/coriander/index.html
