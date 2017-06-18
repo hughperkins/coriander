2017f2b6015fe31374f421fd8167ba7c194067f5
Hugh Perkins <hughperkins@gmail.com>
2017-06-18 09:55:25 +0100

    working on cocl.py

--- bin/cocl.py
+++ bin/cocl.py
@@ -1,14 +1,155 @@
 #!/usr/bin/env python
+
+# This is designed only to run on python 2.7
+# If it happesn to run on python3.5 too, taht's a bonus, but not a design requirements
+
 from __future__ import print_function
 import os
 import platform
 import os
+import sys
 from os import path
 import sys
 
 
-print('foo=bar')
-print('city=paris')
+print('cocl.py args: [%s]' % ' '.join(sys.argv))
+
+DEVICE_PARSE_OPT_LEVEL = os.environ.get('DEVICE_PARSE_OPT_LEVEL', '')
+if DEVICE_PARSE_OPT_LEVEL == '':
+    DEVICE_PARSE_OPT_LEVEL = '2'
+
+DEVICE_PARSE_PASSES = os.environ.get('DEVICE_PARSE_PASSES', '')
+if DEVICE_PARSE_PASSES == '':
+    DEVICE_PARSE_PASSES = 'inline,mem2reg,instcombine'
+
+HOSTSIDE_PARSE_OPT_LEVEL = os.environ.get('HOSTSIDE_PARSE_OPT_LEVEL', '')
+if HOSTSIDE_PARSE_OPT_LEVEL == '':
+    HOSTSIDE_PARSE_OPT_LEVEL = '2'
+
+HOSTSIDE_COMPILE_OPT_LEVEL = os.environ.get('HOSTSIDE_COMPILE_OPT_LEVEL', '')
+if HOSTSIDE_COMPILE_OPT_LEVEL == '':
+    HOSTSIDE_COMPILE_OPT_LEVEL = '3'
+
+DEVICE_PARSE_PASSES_STR = ' '.join(['-%s' % o for o in DEVICE_PARSE_PASSES.split(',')])
+print('DEVICE_PARSE_PASSES_STR [%s]' % DEVICE_PARSE_PASSES_STR)
+
+ADDFLAGS = ''
+NATIVE_COMPILER = 'g++'
+SO_SUFFIX = '.so'
+if platform.uname()[0] == 'Darwin':
+    ADDFLAGS = '-stdlib=libc++'
+    NATIVE_COMPILER = 'clang++'
+    SO_SUFFIX = '.dylib'
+
+def display_help():
+    print("""
+Usage: cocl [options] <targetfile>
+  -c compile to .o only, dont link
+  -o final output filepath
+  --clang-home Path to llvm4.0
+
+  Options passed through to clang compiler:
+    -fPIC
+    -I<INCLUDEDIR>
+    -D<SYMBOL>[=<VALUE>]
+    -g
+
+  Options ignored currently:
+    -O
+    -G
+    -U
+    -M
+    -x <something>
+    -iquote <something>
+    -isystem <something>
+    -compiler-options <something>
+    -fno-canonical-system-headers
+    -std<something>
+    -compiler-bindir
+    -gencode
+""")
+
+PASSTHRU = ''
+IROOPENCLARGS = ''
+COMPILE_ONLY = False
+OPT_G = ''
+OUTPATH = ''
+CLANG_HOME = ''
+COCL_BIN = ''
+INCLUDES = []
+INFILES = []
+
+args = sys.argv[1:]
+while len(args) > 0:
+    THISARG = args[0].split('=')[0]
+    DONE = False
+    TWOLETTERS = args[0][:2]
+    if TWOLETTERS == ';':
+           # ignore (artifact of cmake file hacking...
+           DONE = True
+    elif TWOLETTERS == '-I':
+        # echo got include
+        # need to check if theres a space or not
+        if THISARG == '-I':
+            INCLUDES.append(args[1])
+            args = args[1:]
+        else:
+            INCLUDES.extend([inc.replace('-I', '') for inc in THISARG.split(';')])
+        DONE = True
+    elif TWOLETTERS == '-D':
+        # hacktastic :-D. Would be better to fix the cmake rules, sooner or later...
+        DSTRIPPED = THISARG.REPLACE('-D-D', '-D')
+        PASSTHRU += ' %s' % DSTRIPPED
+        DONE = True
+    elif TWOLETTERS == '-O':
+        # ignore...
+        DONE = True
+    elif TWOLETTERS == '-G':
+        # ignore
+        DONE = True
+    elif TWOLETTERS == '-U':
+        DONE = True
+    elif TWOLETTERS == '-x':
+        # not sure why we are receiving -x cu? ignore for now
+        args = args[1:]
+        DONE = True
+
+    if not DONE:
+        if THISARG == '-c':
+            COMPILE_ONLY = True
+        elif THISARG == '-g':
+            OPT_G = '-g'
+        elif THISARG == '-o':
+            OUTPATH = args[1]
+            args = args[1:]
+        elif THISARG == '--clang-home':
+            CLANG_HOME = args[1]
+            args = args[1:]
+        elif THISARG == '--cocl-bin':
+            COCL_BIN = args[1]
+            args = args[1:]
+        elif THISARG == '--cocl-lib':
+            COCL_LIB = args[1]
+            args = args[1:]
+        elif THISARG == '--cocl-include':
+            COCL_INCLUDE = args[1]
+            args = args[1:]
+        elif THISARG in ['-?', '-h', '-help']:
+            display_help()
+            sys.exit(0)
+        elif THISARG in ['-ferror-limit', '-fPIC']:
+            # pass these through to clang++
+            PASSTHRU += ' %s' % THISARG
+        elif THISARG in ['-gencode', '--compile-bindir', '-fno-canonical-system-headers', '-M', '-std']:
+            # ignore these
+        elif THISARG in ['-iquote', '-isystem', '--compiler-options']:
+            # ignore these, and the following token
+            args = args[1:]
+        else:
+            # add as input file
+            INFILES.append(THISARG)
+    args = args[1:]
+
 
 if 'CLANG_HOME' not in os.environ:
     errors = """
---
+++ bin/cocl_py.cmd
@@ -0,0 +1,17 @@
+rem export SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
+rem echo SCRIPT_DIR ${SCRIPT_DIR}
+
+echo cocl_py.cmd
+
+python ..\bin\cocl.py
+
+rem PYOUT=$(${SCRIPT_DIR}/cocl.py)
+rem pyres=$?
+rem  echo PYOUT $PYOUT
+rem eval ${PYOUT}
+rem if [[ $pyres != 0 ]]; then {
+rem     echo $errors
+rem     exit 1
+rem } fi
+rem  echo $?
+rem  echo errors [$errors]
