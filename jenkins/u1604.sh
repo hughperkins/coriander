62156198031dac001901e1cbac173247d0c332a9
Hugh Perkins <hughperkins@gmail.com>
2017-06-19 12:34:03 +0100

    update jenkins script for install_distro.py

--- jenkins/u1604.sh
+++ jenkins/u1604.sh
@@ -6,8 +6,24 @@ set -e
 set -x
 
 BASEDIR=$(pwd)
+
+(
+    set +e
+    sudo rm -Rf /usr/local/include/cocl
+    sudo rm -Rf /usr/local/bin/cocl*
+    for libname in cocl easycl clew clblast; do {
+        sudo rm -Rf /usr/local/lib${libname}*
+    } done
+    sudo rm -Rf /usr/local/share/cocl
+    sudo rm -Rf /usr/local/include/coriander_plugins
+    sudo rm -Rf /usr/local/lib/coriander_plugins
+    set -e
+)
+
+python2 install_distro.py
+
 hg clone https://bitbucket.org/hughperkins/eigen -b tf-coriander
-mkdir build
+# mkdir build
 cd build
 cmake  -DEIGEN_TESTS=ON -DEIGEN_HOME=${BASEDIR}/eigen ..
 make -j 16
