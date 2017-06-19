81012327ebfc5dc214ae0bd920339f9c7f6bcbdf
Hugh Perkins <hughperkins@gmail.com>
2017-06-20 00:45:50 +0100

    abspath

--- install_distro.py
+++ install_distro.py
@@ -119,7 +119,8 @@ def install_llvm():
         run(['tar', '-xf', filename])
         unzip_name = filename.replace('.tar.xz', '')
         run(['mv', unzip_name, 'llvm-4.0'])
-        llvm_dir = join('soft', 'llvm-4.0')
+        cd_repo_root()
+        llvm_dir = path.abspath('soft', 'llvm-4.0')
         if is_llvm_dir(llvm_dir):
             print('installed llvm ok to [%s]' % join(soft, 'llvm-4.0'))
         else:
