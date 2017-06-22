beece44d4ceef0be7a1354d9679242a40fa8d6c7
Hugh Perkins <hughperkins@gmail.com>
2017-06-22 14:05:38 +0100

    add cudnntraining test, to jenkins, temporarily disable other tests ,in jenknis

--- jenkins/u1604.py
+++ jenkins/u1604.py
@@ -66,6 +66,45 @@ def run(cmdlist):
     return res
 
 
+def run_until(cmdlist, until):
+    """
+    Runs until string until appears in output, then kills the process, without
+    checking return code, and returns the output so far
+    """
+    global current_dir
+    print(' '.join(cmdlist))
+    f_out = open('jenkins-out%s.txt', 'w', buffering=1)
+    f_in = open('jenkins-out%s.txt', 'r', buffering=1)
+    f_in.seek(0)
+    p = subprocess.Popen(cmdlist, cwd=current_dir, stdout=f_out, stderr=subprocess.STDOUT, bufsize=1)
+    res = ''
+    def print_progress():
+        line = f_in.readline()
+        # if not is_py2():
+        #     line = line.decode('utf-8')
+        res_lines = ''
+        while line != '':
+            print(line[:-1])
+            res_lines += line
+            line = f_in.readline()
+            # if not is_py2():
+            #     line = line.decode('utf-8')
+        return res_lines
+        # print(lines)
+    p.poll()
+    while p.returncode is None:
+        res += print_progress()
+        if until in res:
+            p.terminate()
+            return res
+        time.sleep(1)
+        p.poll()
+    res += print_progress()
+    print('p.returncode', p.returncode)
+    assert p.returncode == 0
+    return res
+
+
 def maybe_rmtree(tree_dir):
     if path.isdir(tree_dir):
         if platform.uname()[0] == 'Windows':
@@ -110,26 +149,45 @@ def main(git_branch):
         so_suffix = '.dylib'
     elif platform.uname()[0] == 'Windows':
         so_suffix = '.dll'
-    artifacts_list = []
-    for libname in ['cocl', 'clew', 'clblast', 'easycl']:
-        artifacts_list.append('lib%s%s' % (libname, so_suffix))
-    run(['zip', '../artifacts.zip'] + artifacts_list)
-    run(['make', '-j', '16', 'gtest-tests'])
-    run(['make', '-j', '16', 'endtoend-tests'])
-    run(['make', '-j', '16', 'eigen-tests'])
-    run(['make', 'run-gtest-tests'])
-    run(['make', 'run-endtoend-tests'])
-    run(['make', 'run-eigen-tests'])
+    # artifacts_list = []
+    # for libname in ['cocl', 'clew', 'clblast', 'easycl']:
+    #     artifacts_list.append('lib%s%s' % (libname, so_suffix))
+    # run(['zip', '../artifacts.zip'] + artifacts_list)
+    # run(['make', '-j', '16', 'gtest-tests'])
+    # run(['make', '-j', '16', 'endtoend-tests'])
+    # run(['make', '-j', '16', 'eigen-tests'])
+    # run(['make', 'run-gtest-tests'])
+    # run(['make', 'run-endtoend-tests'])
+    # run(['make', 'run-eigen-tests'])
 
     activate(join(coriander_dir, 'activate'))
-    for plugin in ['coriander-clblast', 'coriander-dnn']:
-        cd(join(coriander_dir, 'git', plugin, 'test'))
-        mkdir('build')
-        cd('build')
-        run(['cmake', '..'])
-        run(['cmake', '--build', '.'])
-        run(['cmake', '--build', '.', '--target', 'tests'])
-        run(['cmake', '--build', '.', '--target', 'run-tests'])
+    # for plugin in ['coriander-clblast', 'coriander-dnn']:
+    #     cd(join(coriander_dir, 'git', plugin, 'test'))
+    #     mkdir('build')
+    #     cd('build')
+    #     run(['cmake', '..'])
+    #     run(['cmake', '--build', '.'])
+    #     run(['cmake', '--build', '.', '--target', 'tests'])
+    #     run(['cmake', '--build', '.', '--target', 'run-tests'])
+
+    cd_repo_root()
+    run(['git', 'clone', '--recursive', 'https://github.com/hughperkins/cudnn-training', '-b', git_branch])
+    cd('cudnn-training')
+    mkdir('build')
+    cd('build')
+    run(['cmake', '..', '-DUSE_CUDA=OFF', '-DUSE_OPENCL=ON'])
+    run(['cmake', '--build', '.'])
+
+    wget('http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz')
+    wget('http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz')
+    wget('http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz')
+    wget('http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz')
+    gunzip('train-images-idx3-ubyte.gz')
+    gunzip('train-labels-idx1-ubyte.gz')
+    gunzip('t10k-images-idx3-ubyte.gz')
+    gunzip('t10k-labels-idx1-ubyte.gz')
+
+    run_until(['./lenet'], until='Training iter 2')
 
 
 if __name__ == '__main__':
