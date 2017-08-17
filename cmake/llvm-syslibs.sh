44df84962af2e76d219509afd95673de0ccce47d
iame6162013 <iame6162013@gmail.com>
2017-08-17 11:34:44 +0200

    [arch] Change -ltinfo to -lcurses in llvm-syslibs.sh
    "~/coriander/soft/llvm-4.0/bin/llvm-config --system-libs core" may return "-ltinfo".
    If "-ltinfo" is returned, the compilation will crash because arch doesn't have libtinfo5 installed, linking to ncurses solves the issue.

--- cmake/llvm-syslibs.sh
+++ cmake/llvm-syslibs.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
 export CLANG_HOME=$1
+
+
+if (($(cat /etc/*-release | grep -ce 'arch') > 0)); then
+  SYSLIBS=$(${CLANG_HOME}/bin/llvm-config --system-libs core | tr -d '\n' | sed -e 's/ -ltinfo/ -lcurses/')
+  echo ${SYSLIBS}
+  exit 0
+fi
+
 SYSLIBS=$(${CLANG_HOME}/bin/llvm-config --system-libs core | tr -d '\n')
 echo ${SYSLIBS}
